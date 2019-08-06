bd5d702be7bccf0bb91c86478f9312dd76de146b
Jeff Pihach <jeff.pihach@canonical.com>
2019-08-06 17:06:27 -0600

    Add redux and create store.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import './index.scss';
+
 import App from './App';
+import rootReducer from './reducers/root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/root.js
@@ -0,0 +1,5 @@
+function rootReducer(state = {}, action) {
+  return state;
+}
+
+export default rootReducer;
