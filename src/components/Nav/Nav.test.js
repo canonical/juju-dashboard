dc49635a0f4d4058fc80a2a0af7b9c80c43a636f
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-09 19:11:34 +0100

    Fix Prettier regressions

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./Nav";
 
-import {shallow} from 'enzyme'
+import { shallow } from "enzyme";
 
 describe("Nav", () => {
   it("renders without crashing", () => {
     shallow(
       <Router>
         <Nav />
-      </Router>,
+      </Router>
     );
   });
 });
--- src/juju/index.js
+++ src/juju/index.js
@@ -1,17 +1,14 @@
-import jujulib from '@canonical/jujulib';
-import modelManager from '@canonical/jujulib/api/facades/model-manager-v5.js';
-import {Bakery} from '@canonical/macaroon-bakery';
-
+import jujulib from "@canonical/jujulib";
+import modelManager from "@canonical/jujulib/api/facades/model-manager-v5.js";
+import { Bakery } from "@canonical/macaroon-bakery";
 
 const options = {
   debug: true,
-  facades: [
-    modelManager
-  ],
+  facades: [modelManager],
   bakery: new Bakery({
     visitPage: resp => {
       // XXX Surface message to UI.
-      console.log('visit this URL to login:', resp.Info.VisitURL);
+      console.log("visit this URL to login:", resp.Info.VisitURL);
     }
   })
 };
@@ -23,13 +20,13 @@ async function loginWithBakery() {
     const juju = await jujulib.connect(controllerURL, options);
     const conn = await juju.login({});
     const modelManager = conn.facades.modelManager;
-    const models = await modelManager.listModels({tag: conn.info.identity});
-    console.log('models', models);
+    const models = await modelManager.listModels({ tag: conn.info.identity });
+    console.log("models", models);
   } catch (error) {
     // XXX Surface error to UI.
-    console.log('unable to connect:', error);
+    console.log("unable to connect:", error);
     return;
   }
 }
 
-export {loginWithBakery}
+export { loginWithBakery };
