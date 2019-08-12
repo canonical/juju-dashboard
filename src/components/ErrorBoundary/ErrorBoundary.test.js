42138ed1e6a0b681da3f500b816d091437896fc8
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-12 14:30:58 +0100

    Add snapshot testing for ErrorBoundary component

--- src/components/ErrorBoundary/ErrorBoundary.test.js
+++ src/components/ErrorBoundary/ErrorBoundary.test.js
@@ -1,16 +1,15 @@
 import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+
 import ErrorBoundary from "./ErrorBoundary";
 
-import { shallow } from "enzyme";
-
 describe("Error Boundary", () => {
-  it("generates a error message when an error is caught", () => {
-    const wrapper = shallow(<ErrorBoundary />);
-    wrapper.setState({
-      hasError: true
-    });
-    expect(wrapper.text()).toEqual(
-      "Error: Something has gone wrong. If this issue persists, please raise an issue on GitHub."
-    );
+  const wrapper = shallow(<ErrorBoundary />);
+  wrapper.setState({
+    hasError: true
+  });
+  it("renders without crashing and matches snapshot", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
