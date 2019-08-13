b1cd69c54203978858dd009897cce451011659e7
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-13 12:07:09 +0100

    Throw an error in ChildComponent rather than mutate state directly

--- src/components/ErrorBoundary/ErrorBoundary.test.js
+++ src/components/ErrorBoundary/ErrorBoundary.test.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
 import ErrorBoundary from "./ErrorBoundary";
 
+function ChildComponent() {
+  return null;
+}
+
 describe("Error Boundary", () => {
   it("renders without crashing and matches snapshot", () => {
-    const wrapper = shallow(<ErrorBoundary />);
-    wrapper.setState({
-      hasError: true
-    });
+    const wrapper = mount(
+      <ErrorBoundary>
+        <ChildComponent />
+      </ErrorBoundary>
+    );
+    const error = new Error("Oh noes!");
+    wrapper.find(ChildComponent).simulateError(error);
     expect(wrapper).toMatchSnapshot();
   });
 });
