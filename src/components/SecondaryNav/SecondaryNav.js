e688c439cbcffcc43dd5cca2a05525358778bada
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-14 11:27:36 +0100

    Add selected styles to SecondaryNav

--- src/components/SecondaryNav/SecondaryNav.js
+++ src/components/SecondaryNav/SecondaryNav.js
@@ -1,28 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./_secondary-nav.scss";
+
 const SecondaryNav = () => {
   return (
-    <ul className="p-list">
-      <li className="p-list__item">
-        <Link to="/">Models</Link>
-      </li>
-      <li className="p-list__item">
-        <Link to="/clouds">Clouds</Link>
-      </li>
-      <li className="p-list__item">
-        <Link to="/kubernetes">Kubernetes</Link>
-      </li>
-      <li className="p-list__item">
-        <Link to="/controllers">Controllers</Link>
-      </li>
-      <li className="p-list__item">
-        <Link to="/usage">Usage</Link>
-      </li>
-      <li className="p-list__item">
-        <Link to="/logs">Logs</Link>
-      </li>
-    </ul>
+    <nav className="p-secondary-nav">
+      <ul className="p-list">
+        <li className="p-list__item is-selected">
+          <Link className="p-list__link" to="/">
+            Models
+          </Link>
+        </li>
+        <li className="p-list__item">
+          <Link className="p-list__link" to="/clouds">
+            Clouds
+          </Link>
+        </li>
+        <li className="p-list__item">
+          <Link className="p-list__link" to="/kubernetes">
+            Kubernetes
+          </Link>
+        </li>
+        <li className="p-list__item">
+          <Link className="p-list__link" to="/controllers">
+            Controllers
+          </Link>
+        </li>
+        <li className="p-list__item">
+          <Link className="p-list__link" to="/usage">
+            Usage
+          </Link>
+        </li>
+        <li className="p-list__item">
+          <Link className="p-list__link" to="/logs">
+            Logs
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
