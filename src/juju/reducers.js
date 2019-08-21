bf90851b49fa3ca8773de10df39a55557f280346
Jeff Pihach <jeff.pihach@canonical.com>
2019-08-21 16:51:31 -0600

    Add a default value to the juju reducer state. Add updateModelStatus reducer.

--- src/juju/reducers.js
+++ src/juju/reducers.js
@@ -2,15 +2,16 @@ import produce from "immer";
 
 import { actionsList } from "./actions";
 
-export default function jujuReducers(state = {}, action) {
-  return produce(state, draftState => {
+export default produce(
+  (draftState, action) => {
     // The below is required because CRA ignores the eslint configuration file
     // during development and adds warnings in the dev console.
     // eslint-disable-next-line default-case
     switch (action.type) {
       case actionsList.updateModelList:
-        const modelList = action.payload.userModels.map(model => {
-          return {
+        const modelList = {};
+        action.payload.userModels.forEach(model => {
+          modelList[model.model.uuid] = {
             lastConnection: model.lastConnection,
             name: model.model.name,
             ownerTag: model.model.ownerTag,
@@ -18,8 +19,14 @@ export default function jujuReducers(state = {}, action) {
             uuid: model.model.uuid
           };
         });
-        draftState.models = { items: modelList };
+        draftState.models.items = modelList;
         break;
+      case actionsList.updateModelStatus:
+        const payload = action.payload;
+        draftState.models.items[payload.modelUUID].status = payload.status;
     }
-  });
-}
+  },
+  {
+    models: { items: {} }
+  }
+);
