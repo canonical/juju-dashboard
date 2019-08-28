b24aeea23b67b03e0ae642e51c8de508d87f7dd9
Jeff Pihach <jeff.pihach@canonical.com>
2019-08-27 20:53:01 -0600

    Remove rendering MainTable from the top level App component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,70 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
+import TableList from "../TableList/TableList";
 import PrimaryNav from "../PrimaryNav/PrimaryNav";
 import SecondaryNav from "../SecondaryNav/SecondaryNav";
-import MainTable from "../MainTable/MainTable";
 
 import "../../scss/_layout.scss";
-
-const MainTableHeaders = [
-  { content: "Status", sortKey: "status" },
-  { content: "Cores", sortKey: "cores", className: "u-align--right" },
-  { content: "RAM", sortKey: "ram", className: "u-align--right" },
-  { content: "Disks", sortKey: "disks", className: "u-align--right" }
-];
-
-const MainTableRows = [
-  {
-    columns: [
-      { content: "Ready", role: "rowheader" },
-      { content: 1, className: "u-align--right" },
-      { content: "1 GiB", className: "u-align--right" },
-      { content: 2, className: "u-align--right" }
-    ],
-    sortData: {
-      status: "ready",
-      cores: 2,
-      ram: 1,
-      disks: 2
-    }
-  },
-  {
-    columns: [
-      { content: "Idle", role: "rowheader" },
-      { content: 1, className: "u-align--right" },
-      { content: "1 GiB", className: "u-align--right" },
-      { content: 2, className: "u-align--right" }
-    ],
-    sortData: {
-      status: "idle",
-      cores: 1,
-      ram: 1,
-      disks: 2
-    }
-  },
-  {
-    columns: [
-      { content: "Waiting", role: "rowheader" },
-      { content: 8, className: "u-align--right" },
-      { content: "3.9 GiB", className: "u-align--right" },
-      { content: 3, className: "u-align--right" }
-    ],
-    sortData: {
-      status: "waiting",
-      cores: 8,
-      ram: 3.9,
-      disks: 3
-    }
-  }
-];
 
 // All following components are placeholders and will be replaced with imports.
 function Models() {
   return (
     <>
       <h2>Models</h2>
-      <MainTable headers={MainTableHeaders} rows={MainTableRows} sortable />
+      <TableList />
     </>
   );
 }
