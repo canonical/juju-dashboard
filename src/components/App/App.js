5d836c904cdfd0296fe0573f05c90db3d3b386e3
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-28 11:27:38 +0100

    Use abstracted components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,8 @@ import TableList from "../TableList/TableList";
 import PrimaryNav from "../PrimaryNav/PrimaryNav";
 import SecondaryNav from "../SecondaryNav/SecondaryNav";
 import MainTable from "../MainTable/MainTable";
-import Shell from "../Shell/Shell";
-import Filter from "../Filter/Filter";
+
+import ModelDetails from "../../containers/ModelDetails/ModelDetails";
 
 import "../../scss/_layout.scss";
 
@@ -34,52 +34,9 @@ function Models() {
 }
 
 function ModelsExample() {
-  const viewFilters = ["all", "apps", "units", "machines", "relations"];
-  const statusFilters = ["all", "maintenance", "blocked"];
   return (
     <Layout>
-      <div className="row">
-        <div className="col-2">
-          <div className="row">
-            <div className="col-1">
-              <h4 className="p-muted-heading">Model</h4>
-              <p>cdk-default</p>
-            </div>
-            <div className="col-1">
-              <h4 className="p-muted-heading">Region</h4>
-              <p>us-east-1</p>
-            </div>
-            <div className="col-1">
-              <h4 className="p-muted-heading">Controller</h4>
-              <p>aws-controller</p>
-            </div>
-            <div className="col-1">
-              <h4 className="p-muted-heading">Version</h4>
-              <p>2.3.7</p>
-            </div>
-            <div className="col-1">
-              <h4 className="p-muted-heading">Cloud</h4>
-              <p>aws</p>
-            </div>
-            <div className="col-1">
-              <h4 className="p-muted-heading">SLA</h4>
-              <p>Unsupported</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-10">
-          <div className="row">
-            <div className="col-5">
-              <Filter label="View" filters={viewFilters} />
-            </div>
-            <div className="col-5">
-              <Filter label="Status" filters={statusFilters} />
-            </div>
-          </div>
-          <MainTable headers={MainTableHeaders} rows={MainTableRows} sortable />
-        </div>
-      </div>
-      <Shell />
+      <ModelDetails />
     </Layout>
   );
 }
