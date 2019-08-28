38f46809fa89366ab99914356433ea8b886c4de2
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-28 12:15:18 +0100

    Add tests for correct filter check

--- src/components/Filter/Filter.test.js
+++ src/components/Filter/Filter.test.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import Filter from "./Filter";
 
 describe("Filter", () => {
+  const filters = ["foo", "bar", "baz", "foobar"];
   it("renders without crashing and matches snapshot", () => {
-    const filters = ["foo", "bar", "baz", "foobar"];
     const wrapper = shallow(<Filter label="Filter:" filters={filters} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("displays correct number of filters", () => {
+    const wrapper = mount(<Filter label="Filter:" filters={filters} />);
+    expect(wrapper.find(".p-button--filter")).toHaveLength(4);
   });
 });
