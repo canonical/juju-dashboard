2a4208f559ed598a29b881699cd47d5aaf2abc11
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-08-28 12:15:04 +0100

    className, not class

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -5,13 +5,13 @@ export default class Filter extends Component {
   render() {
     const { label, filters } = this.props;
     const filterButtons = filters.map(filter => (
-      <li key={filter} class="p-inline-list__item">
+      <li key={filter} className="p-inline-list__item">
         <Button>{filter}</Button>
       </li>
     ));
     return (
-      <ul class="p-inline-list">
-        <li class="p-inline-list__item">{label}</li>
+      <ul className="p-inline-list">
+        <li className="p-inline-list__item">{label}</li>
         {filterButtons}
       </ul>
     );
