d1bf7df1caa8394e191a1430705850eaf1346aea
Jeff Pihach <jeff.pihach@canonical.com>
2019-08-27 21:25:01 -0600

    Use the react-redux hook API instead of connect HOC.

--- src/components/TableList/TableList.js
+++ src/components/TableList/TableList.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
 import MainTable from "../MainTable/MainTable";
 
@@ -17,15 +16,40 @@ function generateTableHeaders() {
   ];
 }
 
-function TableList({ modelData }) {
+function TableList() {
+  const modelData = useSelector(generateModelTableData);
   return (
     <MainTable headers={generateTableHeaders()} rows={modelData} sortable />
   );
 }
 
-TableList.propTypes = {
-  modelData: PropTypes.array.isRequired
-};
+/**
+  Returns the model info and statuses in the proper format for the table data.
+  @param {Object} state The application state.
+  @returns {Object} The formatted table data.
+*/
+function generateModelTableData(state) {
+  const models = state.juju.models;
+  return Object.keys(models).map(modelUUID => {
+    const info = models[modelUUID];
+    const modelStatus = state.juju.modelStatuses[modelUUID];
+    return {
+      columns: [
+        { content: info.name },
+        { content: info.ownerTag.split("@")[0].replace("user-", "") },
+        { content: getStatusValue(modelStatus, "summary") },
+        { content: getStatusValue(modelStatus, "cloudTag") },
+        { content: getStatusValue(modelStatus, "region") },
+        // Fetch the credential name from somewhere.
+        { content: "unavailable" },
+        // Fetch the controller name from somewhere.
+        { content: "unavailable" },
+        // We're not currently able to get a last-accessed or updated from JAAS.
+        { content: "unavailable" }
+      ]
+    };
+  });
+}
 
 /**
   Used to fetch the values from status as it won't be defined when the
@@ -67,43 +91,4 @@ function getStatusValue(status, key) {
   return returnValue;
 }
 
-/**
-  Returns the model info and statuses in the proper format for the table data.
-  @param {Object} modelInfos The modelInfo value for all models from the redux
-    Store.
-  @param {Object} modelStatuses The modelStatus value for all models from the
-    redux Store.
-  @returns {Object} The formatted table data.
-*/
-function generateModelTableData(modelInfos, modelStatuses) {
-  return Object.keys(modelInfos).map(modelUUID => {
-    const info = modelInfos[modelUUID];
-    const status = modelStatuses[modelUUID];
-    return {
-      columns: [
-        { content: info.name },
-        { content: info.ownerTag.split("@")[0].replace("user-", "") },
-        { content: getStatusValue(status, "summary") },
-        { content: getStatusValue(status, "cloudTag") },
-        { content: getStatusValue(status, "region") },
-        // Fetch the credential name from somewhere.
-        { content: "unavailable" },
-        // Fetch the controller name from somewhere.
-        { content: "unavailable" },
-        // We're not currently able to get a last-accessed or updated from JAAS.
-        { content: "unavailable" }
-      ]
-    };
-  });
-}
-
-function mapStateToProps(state) {
-  return {
-    modelData: generateModelTableData(
-      state.juju.models,
-      state.juju.modelStatuses
-    )
-  };
-}
-
-export default connect(mapStateToProps)(TableList);
+export default TableList;
