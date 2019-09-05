ab959dcbb5ff1a62119474bdd2dfb03396323edf
Jeff Pihach <jeff.pihach@canonical.com>
2019-09-05 11:53:57 -0600

    Add initial tests for table list component.

---
+++ src/components/TableList/TableList.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import TableList from "./TableList";
+
+import dataDump from "../../testing/complete-redux-store-dump";
+
+const mockStore = configureStore([]);
+
+test("by default, renders with all table headers and no data", () => {
+  const store = mockStore({
+    juju: {
+      models: {},
+      modelInfo: {},
+      modelStatuses: {}
+    }
+  });
+  const wrapper = mount(
+    <Provider store={store}>
+      <TableList />
+    </Provider>
+  );
+  expect(wrapper.find("th")).toMatchSnapshot();
+  // Should be empty
+  expect(wrapper.find("tbody")).toMatchSnapshot();
+});
+
+test("displays all data from redux store", () => {
+  const store = mockStore(dataDump);
+  const wrapper = mount(
+    <Provider store={store}>
+      <TableList />
+    </Provider>
+  );
+  expect(wrapper).toMatchSnapshot();
+});
