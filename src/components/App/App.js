56952fb02366fc34afd2941c9e2ea55515e48626
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-09-18 12:30:30 +0200

    Lazy load page level components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// Components
-import ErrorBoundary from "components/ErrorBoundary/ErrorBoundary";
+import Loading from "components/Loading/Loading";
 
 // Pages
-import Controllers from "pages/Controllers/Controllers";
-import Logs from "pages/Logs/Logs";
-import Models from "pages/Models/Models";
-import ModelDetails from "pages/Models/Details/ModelDetails";
-import NotFound from "pages/NotFound/NotFound";
-import Usage from "pages/Usage/Usage";
+const Controllers = React.lazy(() => import("pages/Controllers/Controllers"));
+const Logs = React.lazy(() => import("pages/Logs/Logs"));
+const Models = React.lazy(() => import("pages/Models/Models"));
+const ModelDetails = React.lazy(() =>
+  import("pages/Models/Details/ModelDetails")
+);
+const NotFound = React.lazy(() => import("pages/NotFound/NotFound"));
+const Usage = React.lazy(() => import("pages/Usage/Usage"));
 
 function App() {
   return (
     <Router>
-      <ErrorBoundary>
-        <Switch>
+      <Switch>
+        <Suspense fallback={<Loading />}>
           <Route path="/" exact component={Models} />
           <Route path="/models/:id" exact component={ModelDetails} />
           <Route path="/controllers" exact component={Controllers} />
           <Route path="/usage" exact component={Usage} />
           <Route path="/logs" exact component={Logs} />
           <Route component={NotFound} />
-        </Switch>
-      </ErrorBoundary>
+        </Suspense>
+      </Switch>
     </Router>
   );
 }
