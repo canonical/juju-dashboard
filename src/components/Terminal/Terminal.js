668f9f69f17afb34418d4592f4aa372a4a31c11b
Jeff Pihach <jeff.pihach@canonical.com>
2019-09-27 15:58:11 -0600

    Use external setup and cleanup methods for the Terminal setup.

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,24 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Terminal as Xterm } from "xterm";
-import { FitAddon as XtermFitAddon } from "xterm-addon-fit";
+
+import cleanUpTerminal from "./cleanup-terminal";
+import setupTerminal from "./setup-terminal";
 
 import "../../../node_modules/xterm/css/xterm.css";
 import "./_terminal.scss";
 
-let terminalInstance = new Xterm();
-let fitAddon = new XtermFitAddon();
-
-terminalInstance.loadAddon(fitAddon);
-
-const Terminal = () => {
+const Terminal = ({ address, creds }) => {
   const [state, setState] = useState();
   const terminalElement = useRef(null);
 
   useEffect(() => {
-    terminalInstance.open(terminalElement.current);
-    fitAddon.fit();
-    terminalInstance.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
-  }, []);
+    const terminalInstance = setupTerminal(address, creds, terminalElement);
+    return cleanUpTerminal(terminalInstance);
+  }, [address, creds]);
 
   return (
     <div className="terminal">
