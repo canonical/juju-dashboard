28ddf5c354655e25baeab939412f7dd76a69a2dc
Jeff Pihach <jeff.pihach@canonical.com>
2019-09-27 15:57:36 -0600

    Create modules for creating and cleaning up terminal instances.

---
+++ src/components/Terminal/cleanup-terminal.js
@@ -0,0 +1,7 @@
+/**
+  Cleans up the terminal instance
+  @param {Xterm} terminalInstance The terminal instance to tear down.
+ */
+export default function cleanUpTerminal(terminalInstance) {
+  // Disonnect from terminal.
+}
