1d908600d319056f421b4c75dd5342c7f00e4471
Jeff Pihach <jeff.pihach@canonical.com>
2019-09-27 15:09:39 -0600

    Create more functional selectors for fetching user credentials so that they can be composed with reselect.

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -6,18 +6,29 @@ export const isLoggedIn = state =>
   state.root.controllerConnection && state.root.bakery;
 
 /**
-  Pull the users macaroon credentials from state, base64 decode and json parse.
-  @returns {Object} The users macaroons in an object with a `macaroons` key.
-    It's an object because this can be expanded to include user/pass credential
-    keys in the future without changing the data type.
+  Pull the users macaroon credentials from state.
+  @returns {Object} The macaroons extracted from the bakery in state.
 */
 export const getUserCredentials = state => {
-  let decodedMacaroons = {};
+  let storedMacaroons = null;
   if (state.root && state.root.bakery) {
-    const storedMacaroons = state.root.bakery.storage._store._items;
-    Object.keys(storedMacaroons).forEach(key => {
-      decodedMacaroons[key] = JSON.parse(atob(storedMacaroons[key]));
-    });
+    storedMacaroons = state.root.bakery.storage._store._items;
   }
-  return { macaroons: decodedMacaroons };
+  return storedMacaroons;
+};
+
+/**
+  Base64 decode and json parse the supplied macaroons from the bakery.
+  @param {Object} macaroons The macaroons data from the bakery.
+  @returns {Object} The users decoded macaroons.
+*/
+export const getDecodedMacaroons = macaroons => {
+  if (!macaroons) {
+    return null;
+  }
+  let decodedMacaroons = {};
+  Object.keys(macaroons).forEach(key => {
+    decodedMacaroons[key] = JSON.parse(atob(macaroons[key]));
+  });
+  return decodedMacaroons;
 };
