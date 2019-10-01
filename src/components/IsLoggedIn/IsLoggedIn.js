d0f829a6eb0dccd993f938adb0edadf17f6af657
Jeff Pihach <jeff.pihach@canonical.com>
2019-10-01 15:18:19 -0600

    If the user isn't logged in then present them with a login link to do so.

--- src/components/IsLoggedIn/IsLoggedIn.js
+++ src/components/IsLoggedIn/IsLoggedIn.js
@@ -3,9 +3,25 @@ import { useSelector } from "react-redux";
 import { isLoggedIn } from "app/selectors";
 
 const IsLoggedIn = ({ children }) => {
-  const isUserLoggedIn = useSelector(isLoggedIn);
-  if (!isUserLoggedIn) {
-    return <div>Please log in</div>;
+  const userIsLoggedIn = useSelector(isLoggedIn);
+  const visitURL = useSelector(state => {
+    if (!userIsLoggedIn) {
+      const root = state.root;
+      if (root && root.visitURL) {
+        return root.visitURL;
+      }
+    }
+  });
+
+  if (!userIsLoggedIn) {
+    return (
+      <div>
+        Please{" "}
+        <a href={visitURL} rel="noopener noreferrer" target="_blank">
+          log in
+        </a>
+      </div>
+    );
   }
   return children;
 };
