7512a427977629c7a610e7844ff3d4dd2ef0bb09
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-10 13:50:35 +0100

    Setup click handler on each filter
    Fixes https://github.com/canonical-web-and-design/juju-squad/issues/873

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,9 +4,13 @@ import Button from "components/Button/Button";
 import "./_filter.scss";
 
 const Filter = ({ label, filters }) => {
+  const _handleFilter = () => {
+    console.log("Handle filter click");
+  };
+
   const filterButtons = filters.map(filter => (
-    <span key={filter} className="p-filter__item">
-      <Button>{filter}</Button>
+    <span key={filter} className="p-filter__item 1">
+      <Button onClick={() => _handleFilter()}>{filter}</Button>
     </span>
   ));
   return (
