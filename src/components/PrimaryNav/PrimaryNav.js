b47cc01795c37ce4633bd6ea4d228a05b267628b
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-14 15:53:23 +0100

    Add animation to external menu dropdown

--- src/components/PrimaryNav/PrimaryNav.js
+++ src/components/PrimaryNav/PrimaryNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
@@ -13,16 +13,27 @@ const pages = [
 
 const PrimaryNav = () => {
   const currentLocation = window.location.pathname;
+  const [extNavOpen, setExtNavOpen] = useState(false);
   return (
-    <nav className="p-primary-nav">
-      <Link to="/">
-        <img
-          className="p-primary-nav__logo"
-          src="https://assets.ubuntu.com/v1/a559e2c5-jaas-black-orange-hz-hex.svg"
-          alt="JAAS logo"
-        />
-      </Link>
-      <ul className="p-list">
+    <nav
+      className={classNames("p-primary-nav", { "ext-nav-open": extNavOpen })}
+    >
+      <div className="p-primary-nav__header">
+        <Link to="/">
+          <img
+            className="p-primary-nav__logo"
+            src="https://assets.ubuntu.com/v1/a559e2c5-jaas-black-orange-hz-hex.svg"
+            alt="JAAS logo"
+          />
+        </Link>
+        <button
+          className="p-primary-nav__toggle"
+          onClick={() => setExtNavOpen(!extNavOpen)}
+        >
+          <i class="p-icon--contextual-menu"></i>
+        </button>
+      </div>
+      <ul className="p-list external">
         <li className="p-list__item">
           <a href="https://jaas.ai/store">Store</a>
         </li>
