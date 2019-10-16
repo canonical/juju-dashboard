b278eafe8a610fdfd62e87878d4dd5282307d3de
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-16 11:56:34 +0100

    Pull model info data from redux store

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,31 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { getModelUUID, getModelStatus } from "app/selectors";
 
 import "./_info-panel.scss";
 
-const InfoPanel = ({ modelStatus }) => {
+const InfoPanel = () => {
+  const { 0: modelName } = useParams();
+
+  const getModelUUIDMemo = useMemo(() => getModelUUID(modelName), [modelName]);
+  const modelUUID = useSelector(getModelUUIDMemo);
+  const getModelStatusMemo = useMemo(() => getModelStatus(modelUUID), [
+    modelUUID
+  ]);
+  const modelStatusData = useSelector(getModelStatusMemo);
+
   return (
     <div className="info-panel">
       <div className="info-panel__grid">
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">Model</h4>
-          <p data-name="name">{modelStatus.model.name}</p>
+          <p data-name="name">
+            {modelStatusData ? modelStatusData.model.name : ""}
+          </p>
         </div>
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">Region</h4>
-          <p data-name="region">{modelStatus.model.region}</p>
+          <p data-name="region">
+            {modelStatusData ? modelStatusData.model.region : ""}
+          </p>
         </div>
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">Controller</h4>
-          <p data-name="controller">{modelStatus.model.type}</p>
+          <p data-name="controller">
+            {modelStatusData ? modelStatusData.model.type : ""}
+          </p>
         </div>
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">Cloud</h4>
-          <p data-name="cloud">{modelStatus.model.cloudTag}</p>
+          <p data-name="cloud">
+            {modelStatusData ? modelStatusData.model.cloudTag : ""}
+          </p>
         </div>
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">SLA</h4>
-          <p data-name="sla">{modelStatus.model.sla}</p>
+          <p data-name="sla">
+            {modelStatusData ? modelStatusData.model.sla : ""}
+          </p>
         </div>
       </div>
       <img
@@ -34,22 +56,6 @@ const InfoPanel = ({ modelStatus }) => {
       />
     </div>
   );
-};
-
-InfoPanel.defaultProps = {
-  modelStatus: {
-    model: {
-      name: "",
-      region: "",
-      type: "",
-      cloudTag: "",
-      sla: ""
-    }
-  }
-};
-
-InfoPanel.propTypes = {
-  modelStatus: PropTypes.object.isRequired
 };
 
 export default InfoPanel;
