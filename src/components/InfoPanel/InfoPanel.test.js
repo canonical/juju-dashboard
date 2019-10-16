77db80021ca8097e7bb7c4f3fc2f4bbd7430f8f8
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-16 14:01:29 +0100

    Update tests to use redux store

--- src/components/InfoPanel/InfoPanel.test.js
+++ src/components/InfoPanel/InfoPanel.test.js
@@ -1,28 +1,43 @@
 import React from "react";
-import { shallow } from "enzyme";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { mount } from "enzyme";
+import { MemoryRouter } from "react-router";
+import { Route } from "react-router-dom";
+import dataDump from "testing/complete-redux-store-dump";
 
 import InfoPanel from "./InfoPanel";
 
-describe("Info Panel", () => {
-  const modelStatus = {
-    model: {
-      name: "fake-model",
-      region: "eu-ireland",
-      type: "iaas",
-      cloudTag: "cloud-google",
-      sla: "Unsupported"
-    }
-  };
+const mockStore = configureStore([]);
 
+describe("Info Panel", () => {
   it("renders without crashing and matches snapshot", () => {
-    const wrapper = shallow(<InfoPanel />);
-    expect(wrapper).toMatchSnapshot();
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/models/group-test"]}>
+          <Route path="/models/*">
+            <InfoPanel />
+          </Route>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(".info-panel")).toMatchSnapshot();
   });
 
   it("displays correct model status info", () => {
-    const wrapper = shallow(<InfoPanel modelStatus={modelStatus} />);
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/models/group-test"]}>
+          <Route path="/models/*">
+            <InfoPanel />
+          </Route>
+        </MemoryRouter>
+      </Provider>
+    );
     expect(wrapper.find('[data-name="name"]').text()).toStrictEqual(
-      "fake-model"
+      "group-test"
     );
   });
 });
