279d68137c69bc673300bb906dbda6aceaf1c28e
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-17 17:59:31 +0100

    Add header to Models page

--- src/pages/Models/Models.js
+++ src/pages/Models/Models.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 import Layout from "components/Layout/Layout";
+import Header from "components/Header/Header";
 import TableList from "components/TableList/TableList";
+import ButtonToggle from "components/ButtonToggle/ButtonToggle";
+import FilterTags from "components/FilterTags/FilterTags";
+
+import "./_models.scss";
 
 export default function Models() {
   return (
     <Layout>
+      <Header>
+        <div className="models__header">
+          <div className="models__count">
+            258 models: 4 blocked, 1 alert, 253 running
+          </div>
+          <ButtonToggle />
+          <FilterTags />
+          <i className="p-icon--user">Account icon</i>
+        </div>
+      </Header>
       <div className="p-strip is-shallow">
         <div className="row">
           <TableList />
