04a9ce606667c66925ee2a2bcf1235aee0ba47da
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-21 12:49:50 +0100

    Add static filter panel

--- src/components/FilterTags/FilterTags.js
+++ src/components/FilterTags/FilterTags.js
@@ -12,6 +12,44 @@ const FilterTags = () => {
           className="p-filter-tags__input"
         />
       </form>
+      <div className="p-card--highlighted p-filter-panel">
+        <div className="p-filter-panel__section">
+          <h4 className="p-filter-panel__heading">Owner</h4>
+          <ul className="p-list p-filter-panel__list">
+            <li className="p-filter-panel__item is-selected">is-team</li>
+            <li className="p-filter-panel__item">dev-team</li>
+            <li className="p-filter-panel__item">yellow-team</li>
+          </ul>
+        </div>
+        <div className="p-filter-panel__section">
+          <h4 className="p-filter-panel__heading">Cloud</h4>
+          <ul className="p-list p-filter-panel__list">
+            <li className="p-filter-panel__item">AWS</li>
+            <li className="p-filter-panel__item">Google</li>
+          </ul>
+        </div>
+        <div className="p-filter-panel__section">
+          <h4 className="p-filter-panel__heading">Region</h4>
+          <ul className="p-list p-filter-panel__list">
+            <li className="p-filter-panel__item">eu-west-1</li>
+            <li className="p-filter-panel__item">eu-west-2</li>
+          </ul>
+        </div>
+        <div className="p-filter-panel__section">
+          <h4 className="p-filter-panel__heading">Credential</h4>
+          <ul className="p-list p-filter-panel__list">
+            <li className="p-filter-panel__item">cred-1</li>
+            <li className="p-filter-panel__item">cred-2</li>
+          </ul>
+        </div>
+        <div className="p-filter-panel__section">
+          <h4 className="p-filter-panel__heading">Controller</h4>
+          <ul className="p-list p-filter-panel__list">
+            <li className="p-filter-panel__item">prodstack-1</li>
+            <li className="p-filter-panel__item">prodstack-2</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
