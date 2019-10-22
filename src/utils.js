9b22c27b805bb04af06049cd5a1f8d23853774fb
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-22 17:16:52 +0100

    Add src/utils.js

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+/**
+ * This is a function.
+ *
+ * @param {string} str - A string param
+ * @param {string} character on which to split string
+ * @return {array} Array of strings split
+ */
+export function splitStr(str, delimiter) {
+  return str.split(delimiter);
+}
