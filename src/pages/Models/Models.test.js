411f4e52780c6ce11ea0475c9cddff69028dc4a3
Jeff Pihach <jeff.pihach@canonical.com>
2019-10-23 10:29:23 -0600

    Update tests for the new header counts.

--- src/pages/Models/Models.test.js
+++ src/pages/Models/Models.test.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { shallow } from "enzyme";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { mount } from "enzyme";
+import { MemoryRouter } from "react-router";
+import dataDump from "testing/complete-redux-store-dump";
+
 import Models from "./Models";
 
+const mockStore = configureStore([]);
+
 describe("Models page", () => {
-  it("renders without crashing and matches snapshot", () => {
-    const wrapper = shallow(<Models />);
-    expect(wrapper).toMatchSnapshot();
+  it("renders without crashing", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Models />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("Header"));
+    expect(wrapper.find("TableList"));
+  });
+
+  it("has a header which shows the model counts", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Models />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(".models__count").text()).toBe(
+      "8 models: 0 blocked, 2 alerts, 6 running"
+    );
   });
 });
