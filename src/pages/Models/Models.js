3e52f8015bec46e3f506328755606df0bc85b2bf
Jeff Pihach <jeff.pihach@canonical.com>
2019-10-23 10:09:03 -0600

    Show the counts for the model statuses in the header.

--- src/pages/Models/Models.js
+++ src/pages/Models/Models.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import Layout from "components/Layout/Layout";
 import Header from "components/Header/Header";
@@ -6,15 +7,32 @@ import TableList from "components/TableList/TableList";
 import ButtonToggle from "components/ButtonToggle/ButtonToggle";
 import FilterTags from "components/FilterTags/FilterTags";
 
+import { getGroupedModelStatusCounts } from "app/selectors";
+
 import "./_models.scss";
 
+function pluralize(value, string) {
+  if (value && (value === 0 || value > 1)) {
+    return string + "s";
+  }
+  return string;
+}
+
 export default function Models() {
+  const { blocked, alert, running } = useSelector(getGroupedModelStatusCounts);
+  const models = blocked + alert + running;
   return (
     <Layout>
       <Header>
         <div className="models__header">
           <div className="models__count">
-            258 models: 4 blocked, 1 alert, 253 running
+            {`${models} ${pluralize(
+              models,
+              "model"
+            )}: ${blocked} blocked, ${alert} ${pluralize(
+              alert,
+              "alert"
+            )}, ${running} running`}
           </div>
           <ButtonToggle />
           <FilterTags />
