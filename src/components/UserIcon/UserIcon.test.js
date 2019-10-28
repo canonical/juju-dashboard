ad8d4f825e31456b2bd42a5373f330ef2b98bc56
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-28 11:59:43 +0000

    Add test for toggle drop-down

--- src/components/UserIcon/UserIcon.test.js
+++ src/components/UserIcon/UserIcon.test.js
@@ -8,4 +8,17 @@ describe("User Icon", () => {
     const wrapper = shallow(<UserIcon />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("toggles drop-down panel", () => {
+    const wrapper = shallow(<UserIcon />);
+
+    const userIconPanel = ".user-icon__panel";
+    const userIconPanelToggle = ".p-icon--user";
+
+    expect(wrapper.find(userIconPanel).hasClass("is-visible")).toEqual(false);
+    wrapper.find(userIconPanelToggle).simulate("click");
+    expect(wrapper.find(userIconPanel).hasClass("is-visible")).toEqual(true);
+    wrapper.find(userIconPanelToggle).simulate("click");
+    expect(wrapper.find(userIconPanel).hasClass("is-visible")).toEqual(false);
+  });
 });
