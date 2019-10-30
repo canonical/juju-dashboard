53ecef47c85c195b1477ba10d0ff955d74be6891
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-10-30 18:40:43 +0000

    Fix capitalisation error

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,7 +3,7 @@ export const actionsList = {
   storeBakery: "STORE_BAKERY",
   storeVisitURL: "STORE_VISIT_URL",
   updateControllerConnection: "UPDATE_CONTROLLER_CONNECTION",
-  LogOut: "LOG_OUT"
+  logOut: "LOG_OUT"
 };
 
 // Action creators
