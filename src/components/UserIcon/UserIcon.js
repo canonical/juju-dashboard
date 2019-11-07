09116ece27ae6c7b929320c997994d544bad6f75
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-11-07 13:45:06 +0000

    Close user panel when click outside detected

--- src/components/UserIcon/UserIcon.js
+++ src/components/UserIcon/UserIcon.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -10,10 +10,44 @@ import "./_user-icon.scss";
 export default function User() {
   const [userPanelVisibility, setUserPanelVisibility] = useState(false);
   const dispatch = useDispatch();
+  const node = useRef();
+
+  useEffect(() => {
+    const closePanel = () => {
+      setUserPanelVisibility(false);
+    };
+
+    const mouseDown = e => {
+      // Check if click is outside of user panel
+      if (!node.current.contains(e.target)) {
+        // If so, close the panel
+        closePanel();
+      }
+    };
+
+    const keyDown = e => {
+      if (e.keyCode === 27) {
+        // Close panel if Esc keydown detected
+        closePanel();
+      }
+    };
+
+    // Add listener on document to capture click events
+    document.addEventListener("mousedown", mouseDown);
+    // Add listener on document to capture keydown events
+    document.addEventListener("keydown", keyDown);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", mouseDown);
+      document.removeEventListener("keydown", keyDown);
+    };
+  }, []);
+
   return (
     <div
       className="user-icon"
       onClick={() => setUserPanelVisibility(!userPanelVisibility)}
+      ref={node}
     >
       <i className="p-icon--user">Account icon</i>
       <div
