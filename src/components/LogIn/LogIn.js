918b187d52c349f61eedd1cf3b2a2785ecbf2ac0
Jeff Pihach <jeff.pihach@canonical.com>
2019-11-17 22:10:06 -0600

    Use a Button for the connecting state and add the p-card-highlighted class.

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -6,16 +6,26 @@ import logo from "static/images/logo/logo-black-on-white.svg";
 
 import "./_login.scss";
 
-const generateButton = (className, visitURL, label) => (
-  <a
-    className={className}
-    href={visitURL}
-    rel="noopener noreferrer"
-    target="_blank"
-  >
-    {label}
-  </a>
-);
+const generateButton = visitURL => {
+  if (visitURL) {
+    return (
+      <a
+        className="p-button--positive"
+        href={visitURL}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Log in to view your models
+      </a>
+    );
+  } else {
+    return (
+      <button className="p-button--neutral" disabled>
+        Connecting...
+      </button>
+    );
+  }
+};
 
 const LogIn = ({ children }) => {
   const userIsLoggedIn = useSelector(isLoggedIn);
@@ -29,19 +39,15 @@ const LogIn = ({ children }) => {
   });
 
   const button = !useSelector(isConnecting)
-    ? generateButton("p-button", "_#", "Connecting...")
-    : generateButton(
-        "p-button--positive",
-        visitURL,
-        "Log in to view your models"
-      );
+    ? generateButton()
+    : generateButton(visitURL);
 
   if (!userIsLoggedIn) {
     return (
       <>
         <div className="login">
-          <div className="login__inner">
-            <img className="login__logo" src={logo} alt="JAAS logo"></img>
+          <div className="login__inner p-card--highlighted">
+            <img className="login__logo" src={logo} alt="JAAS logo" />
             {button}
           </div>
         </div>
