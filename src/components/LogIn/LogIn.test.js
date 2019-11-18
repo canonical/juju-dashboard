30436b1145a0056d5fc25954a967b293af00f092
Jeff Pihach <jeff.pihach@canonical.com>
2019-11-17 21:18:27 -0600

    Update tests for new connecting UI.

--- src/components/LogIn/LogIn.test.js
+++ src/components/LogIn/LogIn.test.js
@@ -9,31 +9,35 @@ import LogIn from "./LogIn";
 const mockStore = configureStore([]);
 
 describe("LogIn", () => {
-  it("renders a login UI if the user is not logged in", () => {
+  it("renders a 'connecting' message while connecting", () => {
     const store = mockStore({
       root: {}
     });
     const wrapper = mount(
       <Provider store={store}>
-        <LogIn>I should not be rendered</LogIn>
+        <LogIn>App content</LogIn>
+      </Provider>
+    );
+    expect(wrapper.find(".p-button").text()).toBe("Connecting...");
+    expect(wrapper.find("LogIn main").text()).toBe("App content");
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders a login UI if the user is not logged in", () => {
+    const store = mockStore({
+      root: {
+        visitURL: "I am a url"
+      }
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <LogIn>App content</LogIn>
       </Provider>
     );
     expect(wrapper.find(".p-button--positive").text()).toBe(
       "Log in to view your models"
     );
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("renders it's children if the user is logged in", () => {
-    const store = mockStore({
-      root: dataDump.root
-    });
-    const wrapper = mount(
-      <Provider store={store}>
-        <LogIn>Hello world!</LogIn>
-      </Provider>
-    );
-    expect(wrapper.find("LogIn").text()).toBe("Hello world!");
+    expect(wrapper.find("LogIn main").text()).toBe("App content");
     expect(wrapper).toMatchSnapshot();
   });
 });
