b38c926dd2950ce84ccc173e4d94cf109213b2a0
Jeff Pihach <jeff.pihach@canonical.com>
2019-11-19 14:58:47 -0600

    Move connectAndListModels to it's own file.

---
+++ src/app/model-poller.js
@@ -0,0 +1,36 @@
+import { fetchAllModelStatuses, loginWithBakery } from "juju";
+import { fetchModelList } from "juju/actions";
+import { updateControllerConnection } from "app/actions";
+
+export default async function connectAndListModels(reduxStore, bakery) {
+  try {
+    // eslint-disable-next-line no-console
+    console.log("Logging into the Juju controller.");
+    const conn = await loginWithBakery(bakery);
+    reduxStore.dispatch(updateControllerConnection(conn));
+    // eslint-disable-next-line no-console
+    console.log("Fetching model list.");
+    await reduxStore.dispatch(fetchModelList());
+    // eslint-disable-next-line no-console
+    console.log("Fetching model statuses");
+
+    let continuePolling = true;
+    while (continuePolling) {
+      await fetchAllModelStatuses(conn, reduxStore);
+      // Wait 30s then start again.
+      continuePolling = await new Promise(resolve => {
+        setTimeout(() => {
+          // XXX Add ability to toggle true to false to pause polling.
+          resolve(true);
+        }, 30000);
+      });
+      // Fetch the model list again as it may have changed.
+      await reduxStore.dispatch(fetchModelList());
+    }
+  } catch (error) {
+    // XXX Surface error to UI.
+    // XXX Send to sentry.
+    // eslint-disable-next-line no-console
+    console.log("Something went wrong: ", error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -6,20 +6,16 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Bakery, BakeryStorage } from "@canonical/macaroon-bakery";
 
+import App from "components/App/App";
+import rootReducer from "app/root";
+import { storeBakery, storeVisitURL } from "app/actions";
+import connectAndListModels from "app/model-poller";
+
+import jujuReducers from "juju/reducers";
+
 import "./scss/index.scss";
 
-import App from "./components/App/App";
-import rootReducer from "./app/root";
 import * as serviceWorker from "./serviceWorker";
-
-import { fetchAllModelStatuses, loginWithBakery } from "./juju";
-import jujuReducers from "./juju/reducers";
-import { fetchModelList } from "./juju/actions";
-import {
-  storeBakery,
-  storeVisitURL,
-  updateControllerConnection
-} from "./app/actions";
 
 const reduxStore = createStore(
   combineReducers({
@@ -36,39 +32,6 @@ const bakery = new Bakery({
   storage: new BakeryStorage(localStorage, {})
 });
 reduxStore.dispatch(storeBakery(bakery));
-
-async function connectAndListModels(reduxStore, bakery) {
-  try {
-    // eslint-disable-next-line no-console
-    console.log("Logging into the Juju controller.");
-    const conn = await loginWithBakery(bakery);
-    reduxStore.dispatch(updateControllerConnection(conn));
-    // eslint-disable-next-line no-console
-    console.log("Fetching model list.");
-    await reduxStore.dispatch(fetchModelList());
-    // eslint-disable-next-line no-console
-    console.log("Fetching model statuses");
-
-    let continuePolling = true;
-    while (continuePolling) {
-      await fetchAllModelStatuses(conn, reduxStore);
-      // Wait 30s then start again.
-      continuePolling = await new Promise(resolve => {
-        setTimeout(() => {
-          // XXX Add ability to toggle true to false to pause polling.
-          resolve(true);
-        }, 30000);
-      });
-      // Fetch the model list again as it may have changed.
-      await reduxStore.dispatch(fetchModelList());
-    }
-  } catch (error) {
-    // XXX Surface error to UI.
-    // XXX Send to sentry.
-    // eslint-disable-next-line no-console
-    console.log("Something went wrong: ", error);
-  }
-}
 
 connectAndListModels(reduxStore, bakery);
 
