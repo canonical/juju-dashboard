1a59f9bc82e94a2e7de9cb1fd37b98b386ad9253
Jeff Pihach <jeff.pihach@canonical.com>
2019-11-20 15:55:35 -0600

    Update test data to new bakery format.

--- src/juju/actions.test.js
+++ src/juju/actions.test.js
@@ -1,11 +1,19 @@
+import dataDump from "testing/complete-redux-store-dump";
 import * as actions from "./actions";
 
 describe("action creators", () => {
   it("updateModelList", () => {
     const models = [{ model: "data" }];
-    expect(actions.updateModelList(models)).toStrictEqual({
-      type: actions.actionsList.updateModelList,
-      payload: models
-    });
+    const dispatch = jest.fn();
+    const getState = () => dataDump;
+    actions.updateModelList(models)(dispatch, getState);
+    expect(dispatch.mock.calls).toEqual([
+      [
+        {
+          type: actions.actionsList.updateModelList,
+          payload: models
+        }
+      ]
+    ]);
   });
 });
