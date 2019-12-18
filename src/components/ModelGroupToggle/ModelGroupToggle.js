352e56d6b68ffb53efd6681a60170800b3331cba
Jeff Pihach <jeff.pihach@canonical.com>
2019-12-18 11:54:28 -0600

    Generate the buttons in a loop.

--- src/components/ModelGroupToggle/ModelGroupToggle.js
+++ src/components/ModelGroupToggle/ModelGroupToggle.js
@@ -3,39 +3,25 @@ import classNames from "classnames";
 
 import "./_model-group-toggle.scss";
 
+const buttons = ["status", "cloud", "owner"];
+
 const ModelGroupToggle = ({ groupedBy, setGroupedBy }) => {
   return (
     <div className="p-model-group-toggle">
       <div className="p-model-group-toggle__inner">
         <span className="p-model-group-toggle__label">Group by:</span>
         <div className="p-model-group-toggle__buttons">
-          <button
-            className={classNames("p-model-group-toggle__button", {
-              "is-selected": groupedBy === "status"
-            })}
-            value="status"
-            onClick={e => setGroupedBy(e.target.value)}
-          >
-            Status
-          </button>
-          <button
-            className={classNames("p-model-group-toggle__button", {
-              "is-selected": groupedBy === "cloud"
-            })}
-            value="cloud"
-            onClick={e => setGroupedBy(e.target.value)}
-          >
-            Cloud
-          </button>
-          <button
-            className={classNames("p-model-group-toggle__button", {
-              "is-selected": groupedBy === "owner"
-            })}
-            value="owner"
-            onClick={e => setGroupedBy(e.target.value)}
-          >
-            Owner
-          </button>
+          {buttons.map(label => (
+            <button
+              className={classNames("p-model-group-toggle__button", {
+                "is-selected": groupedBy === label
+              })}
+              value={label}
+              onClick={e => setGroupedBy(e.target.value)}
+            >
+              {label}
+            </button>
+          ))}
         </div>
       </div>
     </div>
