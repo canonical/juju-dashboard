b17747ae59573ed39b99d326db322c5b7cc40b10
Jeff Pihach <jeff.pihach@canonical.com>
2019-12-18 15:22:27 -0600

    Added tests for the StatusGroup component.

---
+++ src/components/ModelTableList/StatusGroup.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { MemoryRouter } from "react-router";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import StatusGroup from "./StatusGroup";
+
+import dataDump from "../../testing/complete-redux-store-dump";
+
+const mockStore = configureStore([]);
+
+describe("StatusGroup", () => {
+  it("by default, renders with all table headers and no data", () => {
+    const store = mockStore({
+      root: {},
+      juju: {
+        models: {},
+        modelData: {},
+        modelInfo: {},
+        modelStatuses: {}
+      }
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <StatusGroup />
+      </Provider>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(3);
+    expect(tables.get(0).props.rows).toEqual([]);
+    expect(tables.get(1).props.rows).toEqual([]);
+    expect(tables.get(2).props.rows).toEqual([]);
+  });
+
+  it("displays model data grouped by status from the redux store", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <StatusGroup />
+        </Provider>
+      </MemoryRouter>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(3);
+    expect(tables.get(0).props.rows.length).toEqual(2);
+    expect(tables.get(1).props.rows.length).toEqual(4);
+    expect(tables.get(2).props.rows.length).toEqual(11);
+  });
+});
