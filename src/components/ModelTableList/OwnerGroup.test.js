2aa956b02a72472219c68c65ae86804e943f32ed
Jeff Pihach <jeff.pihach@canonical.com>
2019-12-18 15:22:08 -0600

    Added tests for the OwnerGroup component.

---
+++ src/components/ModelTableList/OwnerGroup.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { MemoryRouter } from "react-router";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import OwnerGroup from "./OwnerGroup";
+
+import dataDump from "../../testing/complete-redux-store-dump";
+
+const mockStore = configureStore([]);
+
+describe("OwnerGroup", () => {
+  it("by default, renders no tables with no data", () => {
+    const store = mockStore({
+      root: {},
+      juju: {
+        models: {},
+        modelData: {},
+        modelInfo: {},
+        modelStatuses: {}
+      }
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <OwnerGroup />
+      </Provider>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(0);
+  });
+
+  it("displays model data grouped by owner from the redux store", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <OwnerGroup />
+        </Provider>
+      </MemoryRouter>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(5);
+    expect(tables.get(0).props.rows.length).toEqual(5);
+    expect(tables.get(1).props.rows.length).toEqual(6);
+    expect(tables.get(2).props.rows.length).toEqual(1);
+    expect(tables.get(3).props.rows.length).toEqual(3);
+    expect(tables.get(4).props.rows.length).toEqual(2);
+  });
+});
