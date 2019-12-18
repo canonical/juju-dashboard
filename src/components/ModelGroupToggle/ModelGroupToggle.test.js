8e8dcd09ad0fb03a26564b54ba454c2478860e32
Jeff Pihach <jeff.pihach@canonical.com>
2019-12-18 14:46:11 -0600

    Add tests for interactions.

--- src/components/ModelGroupToggle/ModelGroupToggle.test.js
+++ src/components/ModelGroupToggle/ModelGroupToggle.test.js
@@ -8,4 +8,26 @@ describe("Model group toggle", () => {
     const wrapper = shallow(<ModelGroupToggle />);
     expect(wrapper.find(".p-model-group-toggle")).toMatchSnapshot();
   });
+
+  it("shows active grouping", () => {
+    const wrapper = shallow(<ModelGroupToggle groupedBy="cloud" />);
+    expect(wrapper.find(".p-model-group-toggle")).toMatchSnapshot();
+  });
+
+  it("calls to set group by on click", () => {
+    const setGroupedBy = jest.fn();
+    const wrapper = shallow(
+      <ModelGroupToggle groupedBy="cloud" setGroupedBy={setGroupedBy} />
+    );
+    expect(
+      wrapper.find(".p-model-group-toggle__button.is-selected").text()
+    ).toBe("cloud");
+    wrapper.find("button[value='owner']").simulate("click", {
+      target: { value: "owner" }
+    });
+    expect(setGroupedBy.mock.calls.length).toBe(1);
+    expect(setGroupedBy.mock.calls[0]).toEqual(["owner"]);
+    // We don't check that the UI updated because it has no internal state.
+    // It requires a parent to re-render it with a new selected group.
+  });
 });
