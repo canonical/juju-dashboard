cfba0e39e433b95dbc62cebf607b7a068b6dc769
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-01-09 17:12:19 +0000

    Add tests for CloudGroup

---
+++ src/components/ModelTableList/CloudGroup.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { MemoryRouter } from "react-router";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import CloudGroup from "./CloudGroup";
+
+import dataDump from "../../testing/complete-redux-store-dump";
+
+const mockStore = configureStore([]);
+
+describe("CloudGroup", () => {
+  it("renders without crashing and matches snapshot", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <CloudGroup />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(wrapper.find(CloudGroup)).toMatchSnapshot();
+  });
+
+  it("by default, renders no tables with no data", () => {
+    const store = mockStore({
+      root: {},
+      juju: {
+        models: {},
+        modelData: {},
+        modelInfo: {},
+        modelStatuses: {}
+      }
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <CloudGroup />
+      </Provider>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(0);
+  });
+
+  it("displays model data grouped by owner from the redux store", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <CloudGroup />
+        </Provider>
+      </MemoryRouter>
+    );
+    const tables = wrapper.find("MainTable");
+    expect(tables.length).toBe(2);
+    expect(tables.get(0).props.rows.length).toEqual(15);
+    expect(tables.get(1).props.rows.length).toEqual(2);
+  });
+});
