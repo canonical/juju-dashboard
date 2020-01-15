694d5558ed3ea005f3772d475e3b1bba9cd8b6ee
Anthony Dillon <me@anthonydillon.com>
2020-01-15 17:45:28 +0000

    Add donut charts to the controller overview

--- src/pages/Controllers/Controllers.js
+++ src/pages/Controllers/Controllers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import Layout from "components/Layout/Layout";
+import DonutChart from "components/DonutChart/DonutChart";
 import MainTable from "@canonical/react-components/dist/components/MainTable/MainTable";
 
 import { getControllerData, getModelCounts } from "app/selectors";
@@ -14,6 +15,11 @@ export default function Controllers() {
   const { machinesCount, applicationCount, unitCount } = useSelector(
     getModelCounts
   );
+  const {
+    machinesBlockedCount,
+    machinesAlertCount,
+    machinesRunningCount
+  } = useSelector(getModelCounts);
 
   const headers = [
     { content: "running", sortKey: "running" },
@@ -45,30 +51,89 @@ export default function Controllers() {
       ]
     }));
 
+  let machinesChartData = {machinesCount, applicationCount, unitCount};
+
   return (
     <Layout>
-      <div className="l-content">
-        <div className="overview">
+      <div className="l-content controllers">
+        <div className="p-strip--light is-shallow overview">
           <div className="row">
-            <div className="col-4 overview__machines">
+            <h5>Model status across controllers</h5>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <DonutChart chartData={machinesChartData} />
               <strong data-test="machine-count">
                 {machinesCount} {pluralize(machinesCount, "machine")}
               </strong>
+              <ul className="p-list p-legend">
+                <li className="p-list__item p-legend__item is-blocked">
+                  Blocked: 9
+                </li>
+                <li className="p-list__item p-legend__item is-alert">
+                  Alerts: 20
+                </li>
+                <li className="p-list__item p-legend__item is-running">
+                  Running: 130
+                </li>
+              </ul>
             </div>
-            <div className="col-4 overview__applications">
+            <div className="col-3 overview__applications">
+              {/* <DonutChart /> */}
               <strong data-test="application-count">
                 {applicationCount} {pluralize(applicationCount, "application")}
               </strong>
+              <ul className="p-list p-legend">
+                <li className="p-list__item p-legend__item is-blocked">
+                  Blocked: 9
+                </li>
+                <li className="p-list__item p-legend__item is-alert">
+                  Alerts: 20
+                </li>
+                <li className="p-list__item p-legend__item is-running">
+                  Running: 130
+                </li>
+              </ul>
             </div>
-            <div className="col-4 overview__units">
+            <div className="col-3 overview__units">
+              {/* <DonutChart /> */}
               <strong data-test="unit-count">
                 {unitCount} {pluralize(unitCount, "unit")}
               </strong>
+              <ul className="p-list p-legend">
+                <li className="p-list__item p-legend__item is-blocked">
+                  Blocked: 9
+                </li>
+                <li className="p-list__item p-legend__item is-alert">
+                  Alerts: 20
+                </li>
+                <li className="p-list__item p-legend__item is-running">
+                  Running: 130
+                </li>
+              </ul>
+            </div>
+            <div className="col-3 overview__units">
+              {/* <DonutChart /> */}
+              <strong data-test="unit-count">
+                {unitCount} {pluralize(unitCount, "unit")}
+              </strong>
+              <ul className="p-list p-legend">
+                <li className="p-list__item p-legend__item is-blocked">
+                  Blocked: 9
+                </li>
+                <li className="p-list__item p-legend__item is-alert">
+                  Alerts: 20
+                </li>
+                <li className="p-list__item p-legend__item is-running">
+                  Running: 130
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="controllers">
-          <div className="l-controllers-table">
+        <div className="p-strip">
+          <div className="row l-controllers-table">
+            <h5>Controller status</h5>
             <MainTable
               className={"u-table-layout--auto"}
               headers={headers}
