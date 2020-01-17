9799793ded491fa20e8a7f85a2528c1b867957f1
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-01-17 19:54:15 +0000

    Add tests for FilterTags

--- src/components/FilterTags/FilterTags.test.js
+++ src/components/FilterTags/FilterTags.test.js
@@ -1,20 +1,70 @@
 import React from "react";
-import { shallow } from "enzyme";
-
+import { MemoryRouter } from "react-router";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import FilterTags from "./FilterTags";
+
+import dataDump from "../../testing/complete-redux-store-dump";
+
+const mockStore = configureStore([]);
 
 describe("FilterTags", () => {
   it("renders without crashing and matches snapshot", () => {
-    const wrapper = shallow(<FilterTags />);
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <FilterTags />
+        </Provider>
+      </MemoryRouter>
+    );
     expect(wrapper.find(".p-filter-tags")).toMatchSnapshot();
   });
 
   it("displays the filter panel when input focused", () => {
-    const wrapper = shallow(<FilterTags />);
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <FilterTags />
+        </Provider>
+      </MemoryRouter>
+    );
     const input = ".p-filter-tags__input";
     const panel = ".p-filter-panel";
     expect(wrapper.find(panel).hasClass("is-visible")).toEqual(false);
     wrapper.find(input).simulate("focus");
     expect(wrapper.find(panel).hasClass("is-visible")).toEqual(true);
+  });
+
+  it("displays correct filter types as per model data", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <FilterTags />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(wrapper.exists('[data-test="owner"]')).toEqual(true);
+    expect(wrapper.exists('[data-test="cloud"]')).toEqual(true);
+    expect(wrapper.exists('[data-test="region"]')).toEqual(true);
+    expect(wrapper.exists('[data-test="credential"]')).toEqual(true);
+  });
+
+  it("displays correct number of filter types as per model data", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Provider store={store}>
+          <FilterTags />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(wrapper.find('[data-test="owner"] li').length).toBe(5);
+    expect(wrapper.find('[data-test="cloud"] li').length).toBe(2);
+    expect(wrapper.find('[data-test="region"] li').length).toBe(4);
+    expect(wrapper.find('[data-test="credential"] li').length).toBe(8);
   });
 });
