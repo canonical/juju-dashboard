bf87ce5ca7353166894b62ea27176a62f2226206
Jeff Pihach <jeff.pihach@canonical.com>
2020-01-22 23:33:13 -0600

    Compute correct position for application icons and place them on the topology.

--- src/components/Topology/Topology.js
+++ src/components/Topology/Topology.js
@@ -3,42 +3,89 @@ import * as d3 from "d3";
 
 import { generateIconPath } from "app/utils";
 
+/**
+  Returns whether the application is a subordinate.
+  @param {Object} app The application status object.
+  @returns {Boolean} If the application is a subordinate.
+*/
 const isSubordinate = app => app.subordinateTo.length > 0;
+
+/**
+  Computes the maximum delta from 0 for both the x and y axis. This is necessary
+  because there are no restrictions on placing applications in a bundle at
+  negative indexes.
+  @param {Object} annotations The annotations object from the model status.
+  @returns {Object} The deltas for x and y in the keys { deltaX, deltaY }.
+*/
+const computePositionDelta = annotations => {
+  let deltaX = null;
+  let deltaY = null;
+
+  if (!annotations) {
+    return { deltaX, deltaY };
+  }
+
+  Object.keys(annotations).forEach(appName => {
+    const { "gui-x": guiX, "gui-y": guiY } = annotations[appName];
+    const x = parseFloat(guiX);
+    const y = parseFloat(guiY);
+    if (!isNaN(x) && (deltaX === null || x < deltaX)) {
+      deltaX = x;
+    }
+    console.log(y, deltaY, y < deltaY);
+    if (!isNaN(y) && (deltaY === null || y < deltaY)) {
+      deltaY = y;
+    }
+  });
+
+  return { deltaX, deltaY };
+};
+
+const applyDelta = (position, delta) =>
+  parseFloat(position) + -parseFloat(delta);
 
 export default ({ modelData, width, height }) => {
   const ref = useRef();
 
+  const { deltaX, deltaY } = computePositionDelta(
+    modelData && modelData.annotations
+  );
+
   const applications =
     (modelData &&
       Object.keys(modelData.applications).map(appName => ({
+        ...modelData.annotations[appName],
         ...modelData.applications[appName],
         name: appName
       }))) ||
     [];
 
   useEffect(() => {
-    const zoom = d3.zoom();
-
     const topo = d3
       .select(ref.current)
       .attr("width", width)
       .attr("height", height)
-      .append("g")
-      .call(zoom)
-      // 2 lines below copied....why do they work?
-      .call(zoom.transform, d3.zoomIdentity.translate(100, 50).scale(0.5))
-      .attr("transform", "translate(100,50) scale(.5,.5)");
+      .append("g");
 
     const appIcons = topo.selectAll(".application").data(applications);
 
-    const appIcon = appIcons.enter().append("g");
+    let gridCount = 0;
+    const appIcon = appIcons
+      .enter()
+      .append("g")
+      .attr("transform", d => {
+        const x =
+          d["gui-x"] !== undefined ? applyDelta(d["gui-x"], deltaX) : gridCount;
+        const y =
+          d["gui-y"] !== undefined ? applyDelta(d["gui-y"], deltaY) : gridCount;
+        gridCount += 130;
+        return `translate(${x}, ${y})`;
+      });
 
     appIcon
       .classed("application", true)
       .attr("data-name", d => d.name)
       .append("circle")
-      .attr("cx", d => (isSubordinate(d) ? 65 : 95))
-      .attr("cy", d => (isSubordinate(d) ? 65 : 95))
       .attr("r", d => (isSubordinate(d) ? 60 : 90))
       .attr("fill", "#f5f5f5")
       .attr("stroke-width", 1)
@@ -49,15 +96,13 @@ export default ({ modelData, width, height }) => {
       .attr("xlink:href", d => generateIconPath(d.charm))
       .attr("width", 96)
       .attr("height", 96)
-      .attr("transform", d =>
-        isSubordinate(d) ? "translate(17, 17)" : "translate(47, 47)"
-      );
+      .attr("transform", "translate(-47,-47)");
 
     appIcons.exit().remove();
 
     return () => {
       topo.remove();
     };
-  }, [applications, height, width]);
+  }, [applications, deltaX, deltaY, height, width]);
   return <svg ref={ref} />;
 };
