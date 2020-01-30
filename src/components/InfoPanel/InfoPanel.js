c8690d3a7a1798ab6cc021c81a23aeea61db6694
Jeff Pihach <jeff.pihach@canonical.com>
2020-01-30 12:31:55 -0600

    Allow the topology to be expanded in a modal.

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,16 +1,29 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import Topology from "components/Topology/Topology";
+import Modal from "@canonical/react-components/dist/components/Modal";
 
 import { getModelUUID, getModelStatus } from "app/selectors";
 import { extractCloudName } from "app/utils";
 
 import "./_info-panel.scss";
 
+const expandedTopologyDimensions = () => {
+  const de = document.documentElement;
+  const vw = Math.max(de.clientWidth, window.innerWidth || 0);
+  const vh = Math.max(de.clientHeight, window.innerHeight || 0);
+  const delta = 300;
+  return {
+    width: vw - delta,
+    height: vh - delta
+  };
+};
+
 const InfoPanel = () => {
   const { 0: modelName } = useParams();
+  const [showExpandedTopology, setShowExpandedTopology] = useState(false);
 
   const getModelUUIDMemo = useMemo(() => getModelUUID(modelName), [modelName]);
   const modelUUID = useSelector(getModelUUIDMemo);
@@ -23,11 +36,25 @@ const InfoPanel = () => {
     ? extractCloudName(modelStatusData.model.cloudTag)
     : "";
 
+  const { width, height } = expandedTopologyDimensions();
+
   return (
     <div className="info-panel">
-      <div className="info-panel__pictogram">
-        <Topology width={300} height={300} modelData={modelStatusData} />
-      </div>
+      {showExpandedTopology ? (
+        <Modal close={() => setShowExpandedTopology(false)} title={false}>
+          <Topology width={width} height={height} modelData={modelStatusData} />
+        </Modal>
+      ) : (
+        <div className="info-panel__pictogram">
+          <Topology width={300} height={300} modelData={modelStatusData} />
+          <i
+            className="p-icon--expand"
+            onClick={() => setShowExpandedTopology(!showExpandedTopology)}
+          >
+            Expand topology
+          </i>
+        </div>
+      )}
       <div className="info-panel__grid">
         <div className="info-panel__grid-item">
           <h4 className="p-muted-heading">Controller</h4>
