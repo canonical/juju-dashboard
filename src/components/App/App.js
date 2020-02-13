ccec6f6b7bc3cc2bd004520b865a0bd7f03f55ce
Jeff Pihach <jeff.pihach@canonical.com>
2020-02-13 14:35:02 -0600

    workaround the ReactGA uninitialized warning.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,10 +13,17 @@ import NotFound from "pages/NotFound/NotFound";
 const baseURL = process.env.REACT_APP_BASE_APP_URL;
 
 function App() {
-  if (process.env.NODE_ENV === "production") {
-    ReactGA.initialize("UA-1018242-68");
-    ReactGA.pageview(window.location.href.replace(window.location.origin, ""));
+  let GAKey = "UA-0000000-00";
+  if (
+    process.env.NODE_ENV === "production" &&
+    !localStorage.getItem("disableAnalytics")
+  ) {
+    GAKey = "UA-1018242-68";
   }
+  // Without initializing all the time the console has warnings about ReactGA
+  // initialization falure so we just initialize it with an invalid key.
+  ReactGA.initialize(GAKey);
+  ReactGA.pageview(window.location.href.replace(window.location.origin, ""));
   return (
     <Router basename={baseURL}>
       <ErrorBoundary>
