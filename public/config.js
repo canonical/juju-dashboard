5da52a6209f8dd6804c84783ef02b38a2f08ad67
Jeff Pihach <jeff.pihach@canonical.com>
2020-03-04 10:09:12 -0600

    Move .env settings into external config.js file so other hosts like Juju can modify it.

---
+++ public/config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+var jaasDashboardConfig = {
+  // API host to allow app to connect and retrieve models
+  baseControllerURL: "jimm.jujucharms.com",
+  // Configurable base url to allow deploying to different paths.
+  baseAppURL: "/",
+  // If true then identity will be provided by a third party provider.
+  identityProviderAvailable: true
+};
