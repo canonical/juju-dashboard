acf3f9762ea7dbaf844d211dd86c4804874a4355
Jeff Pihach <jeff.pihach@canonical.com>
2020-03-04 11:15:49 -0600

    prettier

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,10 +3,12 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-jest.mock("app/use-config-hook", () => jest.fn(() => ({
-  baseControllerURL: "jimm.jujucharms.com",
-  baseAppURL: "/",
-  identityProviderAvailable: true
-})));
+jest.mock("app/use-config-hook", () =>
+  jest.fn(() => ({
+    baseControllerURL: "jimm.jujucharms.com",
+    baseAppURL: "/",
+    identityProviderAvailable: true
+  }))
+);
 
 jest.mock("react-ga");
