5f3ccb24cc48bd75329984c106b52dcf083cea0f
Jeff Pihach <jeff.pihach@canonical.com>
2020-03-04 10:53:50 -0600

    mock the config for tests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,10 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
+jest.mock("app/use-config-hook", () => jest.fn(() => ({
+  baseControllerURL: "jimm.jujucharms.com",
+  baseAppURL: "/",
+  identityProviderAvailable: true
+})));
+
 jest.mock("react-ga");
