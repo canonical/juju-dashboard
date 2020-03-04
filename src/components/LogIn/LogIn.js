ed68bff080ca54ef7381ac23ec0bd9f254cb48f1
Jeff Pihach <jeff.pihach@canonical.com>
2020-03-04 11:49:11 -0600

    Render a different login UI if there is an identity provider or if it requries user/pass.

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,13 +1,70 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { isLoggedIn, isConnecting } from "app/selectors";
+import { isLoggedIn } from "app/selectors";
+
 import Loader from "@canonical/react-components/dist/components/Loader";
 
 import logo from "static/images/logo/logo-black-on-white.svg";
 
+import useConfig from "app/use-config-hook";
+
 import "./_login.scss";
 
-const generateButton = visitURL => {
+export default function LogIn({ children }) {
+  const { identityProviderAvailable } = useConfig();
+  const userIsLoggedIn = useSelector(isLoggedIn);
+
+  if (!userIsLoggedIn) {
+    return (
+      <>
+        <div className="login">
+          <div className="login__inner p-card--highlighted">
+            <img className="login__logo" src={logo} alt="JAAS logo" />
+            {identityProviderAvailable ? (
+              <IdentityProviderForm />
+            ) : (
+              <UserPassForm />
+            )}
+          </div>
+        </div>
+        <main>{children}</main>
+      </>
+    );
+  }
+  return children;
+}
+
+function IdentityProviderForm() {
+  const userIsLoggedIn = useSelector(isLoggedIn);
+
+  const visitURL = useSelector(state => {
+    if (!userIsLoggedIn) {
+      const root = state.root;
+      if (root && root.visitURL) {
+        return root.visitURL;
+      }
+    }
+  });
+
+  return <Button visitURL={visitURL}></Button>;
+}
+
+function UserPassForm() {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" />
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" />
+      <button className="p-button--positive" type="submit">
+        Log in
+      </button>
+    </form>
+  );
+}
+
+
+function Button({ visitURL }) {
   if (visitURL) {
     return (
       <a
@@ -26,37 +83,4 @@ const generateButton = visitURL => {
       </button>
     );
   }
-};
-
-const LogIn = ({ children }) => {
-  const userIsLoggedIn = useSelector(isLoggedIn);
-  const visitURL = useSelector(state => {
-    if (!userIsLoggedIn) {
-      const root = state.root;
-      if (root && root.visitURL) {
-        return root.visitURL;
-      }
-    }
-  });
-
-  const button = !useSelector(isConnecting)
-    ? generateButton()
-    : generateButton(visitURL);
-
-  if (!userIsLoggedIn) {
-    return (
-      <>
-        <div className="login">
-          <div className="login__inner p-card--highlighted">
-            <img className="login__logo" src={logo} alt="JAAS logo" />
-            {button}
-          </div>
-        </div>
-        <main>{children}</main>
-      </>
-    );
-  }
-  return children;
-};
-
-export default LogIn;
+}
