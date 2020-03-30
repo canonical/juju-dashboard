2375e8cd71d32c26dadb1f379261eebdc380e02e
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-03-30 17:11:36 +0100

    Resize terminal

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getMacaroons } from "app/selectors";
-import classNames from "classnames";
+import { FitAddon } from "xterm-addon-fit";
 
 import cleanUpTerminal from "./cleanup-terminal";
 import setupTerminal from "./setup-terminal";
@@ -14,6 +14,14 @@ const Terminal = ({ address, modelName }) => {
   const macaroons = useSelector(getMacaroons);
   const history = useHistory();
 
+  // save default terminal height in the state object
+  let [terminalHeight, setTerminalHeight] = useState(200);
+
+  const fitAddonRef = useRef({ current: null });
+
+  const terminalHeaderHeight = 40; // px
+  const minimumTerminalHeight = 84; // px
+
   useEffect(() => {
     // If the model name is found after a juju switch then
     // switch to that route.
@@ -23,6 +31,7 @@ const Terminal = ({ address, modelName }) => {
       }
     };
     const creds = { macaroons };
+    const fitAddon = new FitAddon();
     const terminalInstance = setupTerminal(
       address,
       creds,
@@ -30,8 +39,39 @@ const Terminal = ({ address, modelName }) => {
       terminalElement,
       switchFound
     );
+    terminalInstance.loadAddon(fitAddon);
+    fitAddonRef.current = fitAddon;
 
-    return cleanUpTerminal(terminalInstance);
+    const resize = e => {
+      const viewPortHeight = window.innerHeight;
+      const mousePosition = e.clientY;
+      const newTerminalHeight =
+        viewPortHeight - mousePosition + terminalHeaderHeight;
+      if (newTerminalHeight >= minimumTerminalHeight) {
+        setTerminalHeight(newTerminalHeight);
+      }
+    };
+
+    const addResizeListener = e => {
+      if (e.target.classList.value === "p-terminal__header") {
+        document.addEventListener("mousemove", resize);
+      }
+    };
+
+    const removeResizeListener = () => {
+      document.removeEventListener("mousemove", resize);
+    };
+
+    // set resize listeners
+    window.addEventListener("mousedown", addResizeListener);
+    window.addEventListener("mouseup", removeResizeListener);
+
+    return () => {
+      cleanUpTerminal(terminalInstance);
+      // remove resize listener
+      window.removeEventListener("mousedown", addResizeListener);
+      window.removeEventListener("mouseup", removeResizeListener);
+    };
     // Because the user can switch the model details UI from within the
     // component the `modelName` passed above will change as the UI
     // updates. This causes the terminal to log in again which is
@@ -39,24 +79,31 @@ const Terminal = ({ address, modelName }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const [terminalVisible, setTerminalVisible] = useState(false);
+  useEffect(() => {
+    fitAddonRef.current.fit();
+  }, [terminalHeight]);
 
   return (
     <div
-      className={classNames("p-terminal", {
-        "is-visible": terminalVisible
-      })}
+      className="p-terminal"
+      style={{
+        height: `${terminalHeight}px`
+      }}
     >
-      <div
-        className="p-terminal__header"
-        onClick={() => setTerminalVisible(!terminalVisible)}
-      >
+      <div className="p-terminal__header">
+        <span className="p-terminal__handle"></span>
         <span>Juju Terminal</span>
         <div className="p-terminal__toggle">
           <i className="p-icon--contextual-menu">Toggle Terminal visibility</i>
         </div>
       </div>
-      <div className="p-terminal__shell" ref={terminalElement}></div>
+      <div
+        className="p-terminal__shell"
+        style={{
+          height: `${terminalHeight - terminalHeaderHeight}px`
+        }}
+        ref={terminalElement}
+      ></div>
     </div>
   );
 };
