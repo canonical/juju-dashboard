02d3383f3d0ed1a733e5e1eec89a21cebb3cbfb1
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-03-31 12:26:07 +0100

    Persist user menu active state between page refreshes

--- src/ui/actions.js
+++ src/ui/actions.js
@@ -1,6 +1,7 @@
 // Action labels
 export const actionsList = {
-  collapsibleSidebar: "TOGGLE_COLLAPSIBLE_SIDEBAR"
+  collapsibleSidebar: "TOGGLE_COLLAPSIBLE_SIDEBAR",
+  userMenuActive: "TOGGLE_USER_MENU"
 };
 
 /**
@@ -9,6 +10,16 @@ export const actionsList = {
 export function collapsibleSidebar(toggle) {
   return {
     type: actionsList.collapsibleSidebar,
+    payload: toggle
+  };
+}
+
+/**
+  Persist user menu visibility between page renders
+*/
+export function userMenuActive(toggle) {
+  return {
+    type: actionsList.userMenuActive,
     payload: toggle
   };
 }
--- src/ui/selectors.js
+++ src/ui/selectors.js
@@ -9,7 +9,21 @@
   @returns {Boolean} If the sidebar is collapsible.
 */
 export const isSidebarCollapsible = state => {
-  if (state && state.ui) {
+  if (state?.ui) {
     return state.ui.collapsibleSidebar;
+  }
+};
+
+/**
+  Checks state to see if the user menu is active.
+  Usage:
+    const isUserMenuActive = useSelector(isUserMenuActive);
+
+  @param {Object} state The application state.
+  @returns {Boolean} If the user menu is active
+*/
+export const isUserMenuActive = state => {
+  if (state?.ui) {
+    return state.ui.userMenuActive;
   }
 };
