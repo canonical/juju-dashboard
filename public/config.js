16b9903836c7c0f1baeff44ca1dcecdf74d96842
Jeff Pihach <jeff.pihach@canonical.com>
2020-04-16 13:33:38 -0600

    Add isJuju config option.

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
+// If a new key is added to this config then be sure to also add the key and
+// value to the golang template in config.js.go.
 // eslint-disable-next-line no-unused-vars
 var jaasDashboardConfig = {
   // API host to allow app to connect and retrieve models
@@ -5,5 +7,9 @@ var jaasDashboardConfig = {
   // Configurable base url to allow deploying to different paths.
   baseAppURL: "/",
   // If true then identity will be provided by a third party provider.
-  identityProviderAvailable: true
+  identityProviderAvailable: true,
+  // Is this application being rendered in Juju and not JAAS. This flag should
+  // only be used for superficial updates like logos. Use feature detection
+  // for other environment features.
+  isJuju: false,
 };
