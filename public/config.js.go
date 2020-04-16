3645f0021766c7ad91e4f960be88c20b4cac2633
Jeff Pihach <jeff.pihach@canonical.com>
2020-04-16 13:33:38 -0600

    Add isJuju config option.

--- public/config.js.go
+++ public/config.js.go
@@ -4,5 +4,9 @@ var jaasDashboardConfig = {
   // Configurable base url to allow deploying to different paths.
   baseAppURL: "{{.baseAppURL}}",
   // If true then identity will be provided by a third party provider.
-  identityProviderAvailable: {{.identityProviderAvailable}}
+  identityProviderAvailable: {{.identityProviderAvailable}},
+  // Is this application being rendered in Juju and not JAAS. This flag should
+  // only be used for superficial updates like logos. Use feature detection
+  // for other environment features.
+  isJuju: {{.isJuju}},
 };
