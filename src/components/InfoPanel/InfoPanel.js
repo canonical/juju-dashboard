d0ade99bd633ee36824ce90e1fc4da58d2459ed7
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-06-03 16:47:13 +0100

    Responsively size topology preview

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -12,25 +12,18 @@ import Topology from "components/Topology/Topology";
 import Modal from "@canonical/react-components/dist/components/Modal";
 
 import { getModelUUID, getModelStatus } from "app/selectors";
-import { extractCloudName, getViewportWidth } from "app/utils";
+import { extractCloudName } from "app/utils";
 import useAnalytics from "hooks/useAnalytics";
 
 import fullScreenIcon from "static/images/icons/fullscreen-icon.svg";
 
 import "./_info-panel.scss";
 
-const infoPanelDimensions = () => {
-  const vw = getViewportWidth();
-  // If changes are made to the 1580px breakpoint below then be sure to update
-  // the same breakpoint in _info-panel.scss.
-  const size = vw >= 1580 ? 300 : 230;
-  return size;
-};
-
 const InfoPanel = () => {
   const { 0: modelName } = useParams();
   const [showExpandedTopology, setShowExpandedTopology] = useState(false);
   const [modalDialogDimensions, setModalDialogDimensions] = useState({});
+  const [infoPanelDimensions, setInfoPanelDimensions] = useState({});
 
   const getModelUUIDMemo = useMemo(() => getModelUUID(modelName), [modelName]);
   const modelUUID = useSelector(getModelUUIDMemo);
@@ -44,13 +37,19 @@ const InfoPanel = () => {
     : "";
 
   const { width: modalWidth, height: modalHeight } = modalDialogDimensions;
-  const topologySize = infoPanelDimensions();
+  const { width: infoPanelWidth } = infoPanelDimensions;
 
   const sendAnalytics = useAnalytics();
   const infoPanelRef = useRef();
 
   // Expanded topology width
   useLayoutEffect(() => {
+    const infoPanelPict = infoPanelRef.current.querySelector(
+      ".info-panel__pictogram"
+    );
+    if (infoPanelPict) {
+      setInfoPanelDimensions(infoPanelPict.getBoundingClientRect());
+    }
     const modalDialog = infoPanelRef.current.querySelector(".p-modal__dialog");
     if (modalDialog) {
       setModalDialogDimensions(modalDialog.getBoundingClientRect());
@@ -87,8 +86,8 @@ const InfoPanel = () => {
       ) : (
         <div className="info-panel__pictogram">
           <Topology
-            width={topologySize}
-            height={topologySize}
+            width={infoPanelWidth + 16} // 1rem of padding
+            height={infoPanelWidth + 16} // 1rem of padding
             modelData={modelStatusData}
             data-test="topology"
           />
