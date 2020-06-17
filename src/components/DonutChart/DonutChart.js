dc0acb5e606bf06144964a18642275ce2ae18b0b
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-06-17 14:28:06 +0100

    Add disabled chart state if no data provided

--- src/components/DonutChart/DonutChart.js
+++ src/components/DonutChart/DonutChart.js
@@ -2,27 +2,36 @@ import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
 const DonutChart = ({ chartData }) => {
-  const ref = useRef();
+  const svgRef = useRef();
   const width = 220;
   const height = 220;
   const margin = 40;
+
+  const isDisabled =
+    (chartData?.blocked || 0) +
+    (chartData?.alert || 0) +
+    (chartData?.running || 0)
+      ? 0 > false
+      : true;
 
   useEffect(() => {
     const radius = Math.min(width, height) / 2 - margin;
 
     const svg = d3
-      .select(ref.current)
+      .select(svgRef.current)
       .attr("width", width)
       .attr("height", height)
       .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    const data = chartData;
+    const data = !isDisabled ? chartData : { disabled: 1 };
 
     const color = d3
       .scaleOrdinal()
       .domain(data)
-      .range(["is-blocked", "is-alert", "is-running"]);
+      .range(
+        !isDisabled ? ["is-blocked", "is-alert", "is-running"] : ["is-disabled"]
+      );
 
     const pie = d3.pie().value(function (d) {
       return d.value;
@@ -42,8 +51,8 @@ const DonutChart = ({ chartData }) => {
     return () => {
       svg.remove();
     };
-  }, [chartData]);
-  return <svg ref={ref} />;
+  }, [chartData, isDisabled]);
+  return <svg ref={svgRef} />;
 };
 
 export default DonutChart;
