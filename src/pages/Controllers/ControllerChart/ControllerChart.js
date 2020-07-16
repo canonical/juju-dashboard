d4a1f60a9037fea8787eb6b46ca7a3485ecc873b
Jeff Pihach <jeff.pihach@canonical.com>
2020-07-16 14:03:03 -0600

    format the legend layout.

--- src/pages/Controllers/ControllerChart/ControllerChart.js
+++ src/pages/Controllers/ControllerChart/ControllerChart.js
@@ -7,20 +7,22 @@ import { pluralize } from "app/utils";
 import "./_controller-chart.scss";
 
 export default function ControllerChart({ chartData, totalLabel }) {
-  const totalCount =
-    chartData.blocked || 0 + chartData.alert || 0 + chartData.running || 0;
+  const totalCount = chartData.blocked + chartData.alert + chartData.running;
   return (
     <div className="p-chart">
       <div className="p-chart__chart">
         <DonutChart chartData={chartData} />
       </div>
       <div className="p-chart__legend">
-        <p>
-          <strong data-test="total-count">
-            {totalCount} {pluralize(totalCount, totalLabel)}
-          </strong>
-        </p>
         <ul className="p-list p-legend">
+          <li
+            className="p-list__item p-legend__item label"
+            data-test="legend-label"
+          >
+            <strong data-test="total-count">
+              {totalCount} {pluralize(totalCount, totalLabel)}
+            </strong>
+          </li>
           <li
             className="p-list__item p-legend__item is-blocked"
             data-test="legend-blocked"
