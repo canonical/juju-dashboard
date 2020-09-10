3d8806cb265bcaace43330acd90585d5b4eac24c
Jeff Pihach <jeff.pihach@canonical.com>
2020-09-10 13:34:36 -0600

    Update label support and naming for props.

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -3,21 +3,21 @@ import classNames from "classnames";
 
 import "./_button-group.scss";
 
-const ButtonGroup = ({ buttons, groupedBy, setGroupedBy }) => {
+const ButtonGroup = ({ buttons, label, activeButton, setActiveButton }) => {
   return (
     <div className="p-button-group">
       <div className="p-button-group__inner">
-        <span className="p-button-group__label">Group by:</span>
+        <span className="p-button-group__label">{label}</span>
         <div className="p-button-group__buttons">
           {buttons.map((label) => (
             <button
-              aria-label={`group by ${label}`}
+              aria-label={`view by ${label}`}
               key={label}
               className={classNames("p-button-group__button", {
-                "is-selected": groupedBy === label,
+                "is-selected": activeButton === label,
               })}
               value={label}
-              onClick={(e) => setGroupedBy(e.currentTarget.value)}
+              onClick={(e) => setActiveButton(e.currentTarget.value)}
             >
               {label}
             </button>
