7e3bcaf44f13e0232d5ddf90c78447c4b7276852
Jeff Pihach <jeff.pihach@canonical.com>
2020-09-10 13:50:05 -0600

    Update the useQueryString to support direct usage by the ButtonGroup component.

--- src/hooks/useQueryString.js
+++ src/hooks/useQueryString.js
@@ -6,29 +6,48 @@ import queryString from "query-string";
  * Stores the supplied value in the query string.
  *
  * @param {String} queryKey The key to use in the query string.
- * @param {String} rootValue If the queryKey value is the same as the rootValue
- *  then do not show any value in the query parameter. This is used for the
- *  default layout view of a component.
+ * @param {String} resetValue If the value matches the queryValue then reset
+ * the queryKey to empty. This is used for button groups that have a default
+ * option. This value is returned as the default value of the queryValue if
+ * it's empty.
+ * @returns {Array} [
+ *   queryValue or resetValue if the queryValue is falsy,
+ *   The function to set the queryValue
+ * ]
  */
-function useQueryString(queryKey, rootValue) {
-  const [queryValue, setQueryValue] = useState(null);
-  const history = useHistory();
+function useQueryString(queryKey, resetValue) {
   const { pathname, search } = useLocation();
+  const history = useHistory();
+  const queryStrings = queryString.parse(search, {
+    arrayFormat: "comma",
+  });
+  const [queryValue, setQueryValue] = useState(queryStrings[queryKey]);
 
   useEffect(() => {
     const queryStrings = queryString.parse(search, {
       arrayFormat: "comma",
     });
+    if (
+      queryStrings[queryKey] === queryValue ||
+      (!queryStrings[queryKey] && queryValue === resetValue)
+    ) {
+      // If they are already the same value then don't push another entry.
+      return;
+    }
     queryStrings[queryKey] = queryValue;
     const updatedQs = queryString.stringify(queryStrings);
     history.push({
       pathname,
-      search:
-        queryValue === rootValue || queryValue === null ? null : updatedQs,
+      search: queryValue === resetValue ? null : updatedQs,
     });
-  }, [queryValue, history, search, pathname, queryKey, rootValue]);
+    // The `search` value is intentionally left of the dependencies array below
+    // as the act of pushing the history updates the search value which puts this
+    // callback into an infinite loop. Passing in queryStrings instead has the
+    // same effect as it's a new object on every parse.
+    // eslint-disable-next-line
+  }, [queryValue, queryKey, resetValue, pathname, history]);
 
-  return [queryValue, setQueryValue];
+  return [queryValue || resetValue, setQueryValue];
 }
 
 export default useQueryString;
