4fc8bec3eedf121516cede0a7960a56b5691fb98
Jeff Pihach <jeff.pihach@canonical.com>
2020-09-10 21:52:43 -0600

    Update props to new naming scheme.

--- src/components/ButtonGroup/ButtonGroup.test.js
+++ src/components/ButtonGroup/ButtonGroup.test.js
@@ -5,16 +5,19 @@ import { MemoryRouter } from "react-router-dom";
 import ButtonGroup from "./ButtonGroup";
 
 describe("ButtonGroup", () => {
-  it("shows active grouping", () => {
+  it("shows active button", () => {
     const wrapper = mount(
       <MemoryRouter>
-        <ButtonGroup buttons={["status", "cloud", "owner"]} groupedBy="cloud" />
+        <ButtonGroup
+          buttons={["status", "cloud", "owner"]}
+          activeButton="cloud"
+        />
       </MemoryRouter>
     );
     expect(wrapper.find(".is-selected").length).toBe(1);
   });
 
-  it("if no grouping is defined then none is selected", () => {
+  it("if no active button is defined then none is selected", () => {
     const wrapper = mount(
       <MemoryRouter>
         <ButtonGroup buttons={["status", "cloud", "owner"]} />
@@ -23,14 +26,14 @@ describe("ButtonGroup", () => {
     expect(wrapper.find(".is-selected")).toEqual({});
   });
 
-  it("calls to set group by on click", () => {
-    const setGroupedBy = jest.fn();
+  it("calls to set active button on click", () => {
+    const setActiveButton = jest.fn();
     const wrapper = mount(
       <MemoryRouter>
         <ButtonGroup
           buttons={["status", "cloud", "owner"]}
-          groupedBy="cloud"
-          setGroupedBy={setGroupedBy}
+          activeButton="cloud"
+          setActiveButton={setActiveButton}
         />
       </MemoryRouter>
     );
@@ -40,8 +43,8 @@ describe("ButtonGroup", () => {
     wrapper.find("button[value='owner']").simulate("click", {
       target: { value: "owner" },
     });
-    expect(setGroupedBy.mock.calls.length).toBe(1);
-    expect(setGroupedBy.mock.calls[0]).toEqual(["owner"]);
+    expect(setActiveButton.mock.calls.length).toBe(1);
+    expect(setActiveButton.mock.calls[0]).toEqual(["owner"]);
     // We don't check that the UI updated because it has no internal state.
     // It requires a parent to re-render it with a new selected group.
   });
