ba68f80e2647c304e2a383a31bd4cbb5326c91bc
Jeff Pihach <jeff.pihach@canonical.com>
2020-09-14 12:12:04 -0600

    Add a special set of words to the pluralizer.

--- src/app/utils.js
+++ src/app/utils.js
@@ -152,16 +152,21 @@ export const extractOwnerName = (tag) => {
 };
 
 /**
-  Returns (s) string if the value is more then one
+  Pluralizes the supplied word based on the provided dataset.
   @param {string} value The integer to be checked
   @param {string} string The item name to be pluralized
   @returns {string} The item pluralized if required
 */
 export const pluralize = (value, string) => {
-  if (value === 0 || value > 1) {
-    return string + "s";
+  const special = {
+    running: "running",
+  };
+  if (value === 1) {
+    return string;
+  } else if (special[string]) {
+    return special[string];
   }
-  return string;
+  return `${string}s`;
 };
 
 /**
