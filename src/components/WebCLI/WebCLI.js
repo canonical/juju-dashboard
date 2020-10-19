ae6180eff516c380600324f8aa0003156a2c98d1
Jeff Pihach <jeff.pihach@canonical.com>
2020-10-19 13:49:23 -0600

    Connect to the websocket for the supplied modeluuid and controllerws and send commands when the form is submitted.

--- src/components/WebCLI/WebCLI.js
+++ src/components/WebCLI/WebCLI.js
@@ -1,18 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./_webcli.scss";
 
-const WebCLI = () => {
+const DEFAULT_PLACEHOLDER = "enter command";
+
+const generateAddress = (controllerWSHost, modelUUID) => {
+  if (!controllerWSHost || !modelUUID) {
+    return null;
+  }
+  return `wss://${controllerWSHost}/model/${modelUUID}/commands`;
+};
+
+const WebCLI = ({ controllerWSHost, credentials, modelUUID }) => {
+  const [connection, setConnection] = useState(null);
+  const [placeholder, setPlaceholder] = useState(DEFAULT_PLACEHOLDER);
+
+  const setDisconnectedPlaceholder = () => {
+    setPlaceholder("no web cli backend available");
+  };
+
+  const setConnectedPlaceholder = () => {
+    setPlaceholder(DEFAULT_PLACEHOLDER);
+  };
+
+  const handleCommandSubmit = (e) => {
+    e.preventDefault();
+    connection.send(
+      JSON.stringify({
+        user: credentials.user,
+        credentials: credentials.password,
+        commands: [e.currentTarget.children.command.value],
+      })
+    );
+  };
+
+  useEffect(() => {
+    const address = generateAddress(controllerWSHost, modelUUID);
+    if (!address) {
+      setDisconnectedPlaceholder();
+      return;
+    } else {
+      setConnectedPlaceholder();
+    }
+    const ws = new WebSocket(address);
+    ws.onopen = () => setConnection(ws);
+    ws.onclose = () => setDisconnectedPlaceholder;
+    ws.onmessage = (e) => {
+      console.log(e);
+    };
+    return () => {
+      ws.close();
+    };
+  }, [controllerWSHost, modelUUID]);
+
   return (
     <div className="webcli">
       <div className="webcli__output"></div>
       <div className="webcli__input">
         <div className="webcli__input-prompt">$ juju</div>
-        <input
-          className="webcli__input-input"
-          type="text"
-          placeholder="enter command"
-        />
+        <form onSubmit={handleCommandSubmit}>
+          <input
+            className="webcli__input-input"
+            type="text"
+            name="command"
+            placeholder={placeholder}
+          />
+        </form>
         <div className="webcli__input-help">
           <i className="p-icon--help is-light" />
         </div>
