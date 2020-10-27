dbc2a256fafa4201792bfb2e00bb99db342cee07
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-10-27 11:08:09 +0000

    Update site wide offline banner
    Fixes #694

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
 
 import "./_banner.scss";
 
-export default function Banner({ isActive, children, type }) {
+export default function Banner({ isActive = true, children, type }) {
+  const [bannerClosed, setBannerClosed] = useState(false);
+
   return (
-    <div className="banner" data-active={isActive} data-type={type}>
+    <div
+      className="banner"
+      data-active={isActive && !bannerClosed}
+      data-type={type}
+    >
       {children}
+      <button
+        className="banner__close"
+        onClick={() => {
+          setBannerClosed(true);
+        }}
+      >
+        <i
+          className={classnames("p-icon--close", {
+            "is-light": type === "positive",
+          })}
+        >
+          Close banner
+        </i>
+      </button>
     </div>
   );
 }
--- src/components/Banner/Banner.test.js
+++ src/components/Banner/Banner.test.js
@@ -27,4 +27,11 @@ describe("Banner", () => {
     const wrapper = shallow(<Banner type="caution" />);
     expect(wrapper.find("div").prop("data-type")).toStrictEqual("caution");
   });
+
+  it("should close if close button is pressed", () => {
+    const wrapper = shallow(<Banner type="caution" />);
+    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(true);
+    wrapper.find(".banner__close").simulate("click");
+    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(false);
+  });
 });
