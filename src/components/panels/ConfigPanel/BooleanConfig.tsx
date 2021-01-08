c9dcc17882628a76b8ba5983d29f7470774391a0
Jeff Pihach <jeff.pihach@canonical.com>
2021-01-08 14:45:11 -0600

    Render the boolean config options.

--- src/components/panels/ConfigPanel/BooleanConfig.tsx
+++ src/components/panels/ConfigPanel/BooleanConfig.tsx
@@ -1,7 +1,63 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
+import classnames from "classnames";
 
 import type { ConfigProps } from "./ConfigPanel";
 
-export default function BooleanConfig({ config }: ConfigProps): ReactElement {
-  return <div className="config-input"></div>;
+export default function BooleanConfig({
+  config,
+  selectedConfig,
+  setSelectedConfig,
+}: ConfigProps): ReactElement {
+  const [inputFocused, setInputFocused] = useState(false);
+  const wrapper = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selectedConfig?.name === config.name) {
+      setInputFocused(true);
+    } else {
+      setInputFocused(false);
+    }
+  }, [selectedConfig, config]);
+
+  const inputName = `${config.name}-booleanSelect`;
+
+  return (
+    // XXX How to tell aria to ignore the click but not the element?
+    // eslint-disable-next-line
+    <div
+      className={classnames("config-input", {
+        "config-input--focused": inputFocused,
+      })}
+      ref={wrapper}
+      onClick={() => setSelectedConfig(config)}
+    >
+      <h5>{config.name}</h5>
+      <div className="row">
+        <label className=".p-radio--inline col-1">
+          <input
+            type="radio"
+            className="p-radio__input"
+            name={inputName}
+            aria-labelledby={inputName}
+            defaultChecked={config.value === true}
+          />
+          <span className="p-radio__label" id={inputName}>
+            true
+          </span>
+        </label>
+        <label className="p-radio--inline col-1">
+          <input
+            type="radio"
+            className="p-radio__input"
+            name={inputName}
+            aria-labelledby={inputName}
+            defaultChecked={config.value === false}
+          />
+          <span className="p-radio__label" id={inputName}>
+            false
+          </span>
+        </label>
+      </div>
+    </div>
+  );
 }
