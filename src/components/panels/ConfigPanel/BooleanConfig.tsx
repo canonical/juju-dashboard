e7deabdc9efc6fc1060e8df0523622d5c13f3b0f
Jeff Pihach <jeff.pihach@canonical.com>
2021-01-08 14:12:46 -0600

    Add wrapper for BooleanConfig component.

---
+++ src/components/panels/ConfigPanel/BooleanConfig.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+import type { ConfigProps } from "./ConfigPanel";
+
+export default function BooleanConfig({ config }: ConfigProps): ReactElement {
+  return <div className="config-input"></div>;
+}
