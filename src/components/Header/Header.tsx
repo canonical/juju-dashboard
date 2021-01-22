7b7bc5e11acab0cfc004198e71c4b7e05bde3c23
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-01-22 10:12:48 +0000

    Revert function typing in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
-import { FunctionComponent } from "react";
-
 import "./_header.scss";
 
 type Props = {
   children: JSX.Element;
 };
 
-const Header: FunctionComponent<Props> = ({ children }) => {
+const Header = ({ children }: Props) => {
   return <div className="header">{children}</div>;
 };
 
