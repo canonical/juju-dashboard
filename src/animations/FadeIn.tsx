d64a1fa0ea5efa41ae0bfa767574903fe5ffb7a5
Jeff Pihach <jeff.pihach@canonical.com>
2021-01-25 15:03:28 -0600

    Allow custom class names to be passed in.

--- src/animations/FadeIn.tsx
+++ src/animations/FadeIn.tsx
@@ -3,18 +3,24 @@ import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {
   children: ReactElement | ReactElement[];
+  className?: string;
   isActive: boolean;
 };
 
 export default function FadeIn({
   children,
+  className,
   isActive = true,
 }: Props): ReactElement {
   return (
     <>
       {isActive && (
         <AnimatePresence>
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className={className}
+          >
             {children}
           </motion.div>
         </AnimatePresence>
