6a8b2480a72169f780735032fcb984c1648203b7
Jeff Pihach <jeff.pihach@canonical.com>
2021-02-03 14:29:11 -0600

    focus the neutral button in the modal when opened.

--- src/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 import { Modal } from "@canonical/react-components/dist/components/Modal/Modal";
@@ -14,13 +14,32 @@ export default function ConfirmationModal({
   body,
   buttonRow,
 }: Props): ReactElement {
+  const modalRef = useRef<HTMLDivElement>(null);
   const portalHost =
     document.querySelector("#confirmation-modal-container") || document.body;
 
+  useEffect(() => {
+    if (modalRef.current) {
+      const neutralButton: HTMLButtonElement | null = modalRef?.current?.querySelector(
+        "button.p-button--neutral"
+      );
+      // If the modal has a neutral button then focus on that after opening
+      // to make a better keyboard navigatione experience.
+      if (neutralButton) {
+        neutralButton.focus();
+      } else {
+        // If there is no button then focus on the modal wrapper.
+        modalRef.current.focus();
+      }
+    }
+  }, []);
+
   return createPortal(
-    <Modal buttonRow={buttonRow}>
-      <div>{body}</div>
-    </Modal>,
+    <div ref={modalRef}>
+      <Modal buttonRow={buttonRow}>
+        <div>{body}</div>
+      </Modal>
+    </div>,
     portalHost
   );
 }
