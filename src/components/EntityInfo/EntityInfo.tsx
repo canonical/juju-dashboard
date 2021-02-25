4e545dc5cc82d28d84c49a42b0b93de366c350d8
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-02-25 15:34:16 +0000

    Add EntityInfo to InfoPanel

---
+++ src/components/EntityInfo/EntityInfo.tsx
@@ -0,0 +1,22 @@
+import "./_entity-info.scss";
+
+type Props = {
+  data: {
+    label: string;
+    value: string;
+  };
+};
+export default function EntityInfo({ data }: Props): JSX.Element {
+  return (
+    <div className="entity-info__grid">
+      {Object.entries(data).map(([label, value]) => {
+        return (
+          <div className="entity-info__grid-item" key={label}>
+            <h4 className="p-muted-heading">{label}</h4>
+            <p data-name={label}>{value}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
