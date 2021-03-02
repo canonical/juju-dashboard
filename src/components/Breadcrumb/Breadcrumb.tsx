1e749223e24e6fd3e4c727b1778e2a8c0f06d409
Anthony Dillon <me@anthonydillon.com>
2021-03-02 20:27:13 +0000

    Create util function to generate the model URL

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -6,28 +6,24 @@ import type { EntityDetailsRoute } from "components/Routes/Routes";
 export default function Breadcrumb(): JSX.Element {
   const { userName, modelName, appName } = useParams<EntityDetailsRoute>();
 
+  const generateModelURL = function (): string {
+    if (userName) {
+      return `/models/${userName}/${modelName}`;
+    } else {
+      return `/models/${modelName}`;
+    }
+  };
+
   return (
     <nav className="p-breadcrumbs" aria-label="Breadcrumb navigation">
       <ol className="p-breadcrumbs__items" data-test="breadcrumb-items">
         {appName ? (
           <>
             <li className="p-breadcrumbs__item" data-test="breadcrumb-model">
-              <Link
-                href={`/models/${
-                  userName ? `${userName}/${modelName}` : `${modelName}`
-                }`}
-              >
-                {modelName}
-              </Link>
+              <Link href={generateModelURL()}>{modelName}</Link>
             </li>
             <li className="p-breadcrumbs__item" data-test="breadcrumb-section">
-              <Link
-                href={`/models/${
-                  userName ? `${userName}/${modelName}` : `${modelName}`
-                }`}
-              >
-                Applications
-              </Link>
+              <Link href={generateModelURL()}>Applications</Link>
             </li>
             <li
               className="p-breadcrumbs__item"
