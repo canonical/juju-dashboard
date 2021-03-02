ed383f664cb09867d0abc7d89481a44a2d94fd3d
Anthony Dillon <me@anthonydillon.com>
2021-03-02 17:52:20 +0000

    Implement the breadcrumb component in the header

---
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,53 @@
+import { Link } from "@canonical/react-components";
+import { useParams } from "react-router-dom";
+
+export type EntityDetailsRoute = {
+  userName: string;
+  modelName: string;
+  appName: string;
+};
+
+const Breadcrumb = (): JSX.Element => {
+  const { userName, modelName, appName } = useParams<EntityDetailsRoute>();
+
+  return (
+    <nav className="p-breadcrumbs" aria-label="Breadcrumb navigation">
+      <ol className="p-breadcrumbs__items">
+        {appName ? (
+          <>
+            <li className="p-breadcrumbs__item" data-test="breadcrumb-model">
+              <Link
+                href={`/models/${
+                  userName ? `${userName}/${modelName}` : `${modelName}`
+                }`}
+              >
+                {modelName}
+              </Link>
+            </li>
+            <li className="p-breadcrumbs__item" data-test="breadcrumb-section">
+              <Link
+                href={`/models/${
+                  userName ? `${userName}/${modelName}` : `${modelName}`
+                }`}
+              >
+                Applications
+              </Link>
+            </li>
+            <li
+              className="p-breadcrumbs__item"
+              data-test="breadcrumb-application"
+            >
+              <strong>{appName}</strong>
+            </li>
+          </>
+        ) : (
+          <li className="p-breadcrumbs__item" data-test="breadcrumb-model">
+            <strong>{modelName}</strong>
+          </li>
+        )}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
