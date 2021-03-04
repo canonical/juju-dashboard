a25c627cba0ad19c89503762ec8de6f78b47453a
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-03-04 13:27:49 +0000

    Refactor Breadcrumbs for testability

--- src/components/Breadcrumb/Breadcrumb.test.tsx
+++ src/components/Breadcrumb/Breadcrumb.test.tsx
@@ -51,7 +51,63 @@ describe("Breadcrumb", () => {
       wrapper.find("[data-test='breadcrumb-section']").text()
     ).toStrictEqual("Applications");
     expect(
-      wrapper.find("[data-test='breadcrumb-application']").text()
+      wrapper.find("[data-test='breadcrumb-applications']").text()
     ).toStrictEqual("easyrsa");
+  });
+
+  it("displays correctly on unit details", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[
+            "/models/eggman@external/group-test/unit/logstash-0",
+          ]}
+        >
+          <TestRoute path="/models/:userName/:modelName?/unit/:unitId?">
+            <Breadcrumb />
+          </TestRoute>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("[data-test='breadcrumb-items']").text()).toStrictEqual(
+      "group-testUnitslogstash-0"
+    );
+    expect(wrapper.find("[data-test='breadcrumb-model']").text()).toStrictEqual(
+      "group-test"
+    );
+    expect(
+      wrapper.find("[data-test='breadcrumb-section']").text()
+    ).toStrictEqual("Units");
+    expect(wrapper.find("[data-test='breadcrumb-units']").text()).toStrictEqual(
+      "logstash-0"
+    );
+  });
+
+  it("displays correctly on machine details", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={["/models/eggman@external/group-test/machine/0"]}
+        >
+          <TestRoute path="/models/:userName/:modelName?/machine/:machineId?">
+            <Breadcrumb />
+          </TestRoute>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("[data-test='breadcrumb-items']").text()).toStrictEqual(
+      "group-testMachines0"
+    );
+    expect(wrapper.find("[data-test='breadcrumb-model']").text()).toStrictEqual(
+      "group-test"
+    );
+    expect(
+      wrapper.find("[data-test='breadcrumb-section']").text()
+    ).toStrictEqual("Machines");
+    expect(
+      wrapper.find("[data-test='breadcrumb-machines']").text()
+    ).toStrictEqual("0");
   });
 });
