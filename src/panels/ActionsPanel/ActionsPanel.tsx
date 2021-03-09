659815352f29aa650d570e2c1f8f87a5e2aba879
Jeff Pihach <jeff.pihach@canonical.com>
2021-03-09 16:38:14 -0600

    Use the PanelHeader in the ActionsPanel.

--- src/panels/ActionsPanel/ActionsPanel.tsx
+++ src/panels/ActionsPanel/ActionsPanel.tsx
@@ -1,9 +1,13 @@
 import Aside from "components/Aside/Aside";
+import PanelHeader from "components/PanelHeader/PanelHeader";
 
 export default function ActionsPanel(): JSX.Element {
+  const title = <div>0 3 units selected</div>;
   return (
-    <Aside>
-      <div className="actions-panel">Actions</div>
+    <Aside width="narrow">
+      <div className="p-panel actions-panel">
+        <PanelHeader title={title} />
+      </div>
     </Aside>
   );
 }
