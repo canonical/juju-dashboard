0a777d792952d829a4605b054af6a3f3e7b1c21e
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-03-10 11:58:27 +0000

    Close panel if Escape key clicked
    Fixes https://github.com/canonical-web-and-design/jaas-dashboard/issues/914

--- src/panels/panels.tsx
+++ src/panels/panels.tsx
@@ -1,12 +1,21 @@
 import { useQueryParam, StringParam } from "use-query-params";
 import { AnimatePresence } from "framer-motion";
+import useEventListener from "hooks/useEventListener";
 
 import RegisterController from "components/RegisterController/RegisterController";
 
 import "./_panels.scss";
 
 export default function Panels() {
-  const panelQs = useQueryParam("panel", StringParam)[0];
+  const [panelQs, setPanelQs] = useQueryParam("panel", StringParam);
+
+  // Close panel if Escape key is pressed when panel active
+  const closeOnEscape = (e: KeyboardEvent) => {
+    if (e.code === "Escape") {
+      setPanelQs(undefined);
+    }
+  };
+  useEventListener("keydown", closeOnEscape);
 
   const generatePanel = () => {
     switch (panelQs) {
