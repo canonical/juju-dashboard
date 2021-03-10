af60a281d6150c8225829649d22caf1719781e66
Jeff Pihach <jeff.pihach@canonical.com>
2021-03-10 16:33:15 -0600

    When clicking the radio, show the description.

--- src/components/RadioInputBox/RadioInputBox.tsx
+++ src/components/RadioInputBox/RadioInputBox.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import classnames from "classnames";
 import "./_radio-input-box.scss";
 
 type Props = {
@@ -9,15 +11,24 @@ export default function RadioInputBox({
   name,
   description,
 }: Props): JSX.Element {
+  const [opened, setOpened] = useState<boolean>(false);
+
+  const handleSelect = (e: any) => {
+    const bool = e.target.value === "on" ? true : false;
+    setOpened(bool);
+  };
   const labelId = `actionRadio-${name}`;
+
   return (
-    <div className="radio-input-box">
+    <div className={classnames("radio-input-box", { opened })}>
       <label className="p-radio">
         <input
           type="radio"
           className="p-radio__input"
           name="actionRadioSelector"
           aria-labelledby={labelId}
+          onClick={handleSelect}
+          onChange={handleSelect}
         />
         <span className="p-radio__label" id={labelId}>
           {name}
