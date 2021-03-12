7d949916e9881739e05ac05a7c48b52543b7d0f6
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-03-12 15:46:39 +0000

    Disable Escape key element if inside form

--- src/panels/panels.tsx
+++ src/panels/panels.tsx
@@ -1,6 +1,7 @@
 import { useQueryParam, StringParam } from "use-query-params";
 import { AnimatePresence } from "framer-motion";
 import useEventListener from "hooks/useEventListener";
+import { TSFixMe } from "types";
 
 import RegisterController from "components/RegisterController/RegisterController";
 
@@ -8,11 +9,19 @@ import "./_panels.scss";
 
 // Close panel if Escape key is pressed when panel active
 export const close = {
-  onEscape: function (
-    e: KeyboardEvent,
-    queryStringSetter: (qs: undefined) => void
-  ) {
-    if (e.code === "Escape") {
+  onEscape: function (e: TSFixMe, queryStringSetter: (qs: undefined) => void) {
+    const targetType = e.target?.tagName.toLowerCase();
+    let isTargetFormElement;
+    switch (targetType) {
+      case "form":
+      case "input":
+      case "textarea":
+      case "select":
+      case "option":
+        isTargetFormElement = true;
+    }
+
+    if (e.code === "Escape" && !isTargetFormElement) {
       queryStringSetter(undefined);
     }
   },
