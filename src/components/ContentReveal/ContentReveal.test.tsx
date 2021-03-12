2016761d5e7615cb3f2c2631c96b92e011f315c0
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-03-12 13:38:54 +0000

    Add snapshot to verify React components as part of ContentReveal title

--- src/components/ContentReveal/ContentReveal.test.tsx
+++ src/components/ContentReveal/ContentReveal.test.tsx
@@ -12,7 +12,7 @@ describe("Content Reveal", () => {
     expect(wrapper.find("p").text()).toStrictEqual("Banner text");
   });
 
-  it("should show correct title", () => {
+  it("should show correct title if only text", () => {
     const wrapper = mount(
       <ContentReveal title="Foo bar" openByDefault={false}>
         <p>Banner text</p>
@@ -21,6 +21,19 @@ describe("Content Reveal", () => {
     expect(wrapper.find(".content-reveal__title").text()).toStrictEqual(
       "Foo bar"
     );
+  });
+
+  it("should show correct title if only text and JSX", () => {
+    const Component = () => <div className="component">component content</div>;
+    const getComponent = () => {
+      return Component;
+    };
+    const wrapper = mount(
+      <ContentReveal title={`Foo bar ${getComponent()}`} openByDefault={false}>
+        <p>Banner text</p>
+      </ContentReveal>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should open by default if set", () => {
