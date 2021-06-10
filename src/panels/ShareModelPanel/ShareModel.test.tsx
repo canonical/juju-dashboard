fd7162f360695ecdded2e321ba120ae237eef968
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-06-10 12:04:52 +0100

    Add small screen view toggles for sharing

--- src/panels/ShareModelPanel/ShareModel.test.tsx
+++ src/panels/ShareModelPanel/ShareModel.test.tsx
@@ -28,4 +28,24 @@ describe("Share Model Panel", () => {
     const panelHeader = wrapper.find(".aside-split-col h5");
     expect(panelHeader.text()).toEqual("Sharing with:");
   });
+
+  it("should show small screen view toggles", () => {
+    const store = mockStore(dataDump);
+    const wrapper = mount(
+      <MemoryRouter initialEntries={["/models/eggman@external/hadoopspark"]}>
+        <Provider store={store}>
+          <TestRoute path="/models/:userName/:modelName?">
+            <QueryParamProvider ReactRouterRoute={Route}>
+              <ShareModel />
+            </QueryParamProvider>
+          </TestRoute>
+        </Provider>
+      </MemoryRouter>
+    );
+    const addNewUserButton = wrapper.find(".share-cards__heading button");
+    expect(addNewUserButton.exists()).toBe(true);
+    addNewUserButton.simulate("click");
+    const backToCardsButton = wrapper.find(".title-wrapper button");
+    expect(backToCardsButton.exists()).toBe(true);
+  });
 });
