2196d14d071ddb011adf12de77d9b545f166df67
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-06-16 16:46:48 +0100

    Add query param check in useModelStatus

--- src/hooks/useModelStatus.js
+++ src/hooks/useModelStatus.js
@@ -2,10 +2,22 @@ import { useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { getModelUUID, getModelStatus } from "app/selectors";
 import { useSelector } from "react-redux";
+import { useQueryParams, StringParam } from "use-query-params";
 
 // Return model status data based on model name in URL
 export default function useModelStatus() {
-  const { modelName } = useParams();
+  let { modelName } = useParams();
+
+  // if model name cannot be derived from URL params, fallback and check for
+  // query string value.
+  const queryParams = useQueryParams({
+    model: StringParam,
+  });
+
+  if (!modelName) {
+    modelName = queryParams[0].model;
+  }
+
   const getModelUUIDMemo = useMemo(() => getModelUUID(modelName), [modelName]);
   const modelUUID = useSelector(getModelUUIDMemo);
   const getModelStatusMemo = useMemo(
