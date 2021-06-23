fcbd7187398115f6042002b51b00b338c9c92678
Barry McGee <barry.mcgee@integralcode.co.uk>
2021-06-23 11:16:20 +0100

    Add test for removing query params when closing panel

--- src/components/PanelHeader/PanelHeader.test.tsx
+++ src/components/PanelHeader/PanelHeader.test.tsx
@@ -1,6 +1,7 @@
 import { mount } from "enzyme";
 import { QueryParamProvider } from "use-query-params";
-import { MemoryRouter, Route } from "react-router";
+import { MemoryRouter, Router, Route } from "react-router";
+import { createMemoryHistory } from "history";
 
 import PanelHeader from "./PanelHeader";
 
@@ -17,5 +18,25 @@ describe("PanelHeader", () => {
       </MemoryRouter>
     );
     expect(wrapper.find(".p-panel__title").contains(title)).toBe(true);
+  });
+
+  it("Removes all query params when close button clicked", () => {
+    const history = createMemoryHistory();
+    history.push("/models?model=cmr&panel=share-model");
+
+    const wrapper = mount(
+      <Router history={history}>
+        <QueryParamProvider ReactRouterRoute={Route}>
+          <PanelHeader title="Title" />
+        </QueryParamProvider>
+      </Router>
+    );
+    const searchParams = new URLSearchParams(history.location.search);
+    expect(searchParams.get("panel")).toEqual("share-model");
+    expect(searchParams.get("model")).toEqual("cmr");
+    wrapper.find(".js-aside-close").simulate("click");
+    const searchParamsAfterClose = new URLSearchParams(history.location.search);
+    expect(searchParamsAfterClose.get("panel")).toEqual(null);
+    expect(searchParamsAfterClose.get("model")).toEqual(null);
   });
 });
