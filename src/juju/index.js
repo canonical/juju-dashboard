4aa0f8bd49f0111db9aed069b12655b4da66b647
Jeff Pihach <jeff.pihach@canonical.com>
2021-07-13 15:32:19 -0600

    Fail silently if we're unable to make a fullStatus request.

--- src/juju/index.js
+++ src/juju/index.js
@@ -186,6 +186,15 @@ async function fetchModelStatus(modelUUID, wsControllerURL, getState) {
       );
       if (isLoggedIn(wsControllerURL, getState())) {
         status = await conn.facades.client.fullStatus();
+        if (status.error) {
+          // XXX If there is an error fetching the full status it's likely that
+          // Juju can no longer access this model. At this moment we don't have
+          // a location to notify the user. In the new watcher model that's
+          // being implemented we will be able to surface this error in the
+          // model details page.
+          console.log(status.error);
+          return;
+        }
       }
       if (isLoggedIn(wsControllerURL, getState())) {
         const entities = Object.keys(status.applications).map((name) => ({
