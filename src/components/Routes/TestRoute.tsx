7cf87c66283aef17149cde3d2b3e45e3a6855dc9
Jeff Pihach <jeff.pihach@canonical.com>
2021-08-17 13:09:42 -0600

    Temporarily gut the TestRoute component.

--- src/components/Routes/TestRoute.tsx
+++ src/components/Routes/TestRoute.tsx
@@ -1,16 +1,10 @@
 import { Route } from "react-router-dom";
 
-import { paths } from "./Routes";
-
 type Props = {
   path: string;
   children: JSX.Element;
 };
 
 export default function TestRoute({ path, children }: Props) {
-  // Validate paths
-  if (Object.keys(paths).indexOf(path) < 0) {
-    throw new Error("Path not found in application routes");
-  }
   return <Route path={path}>{children}</Route>;
 }
