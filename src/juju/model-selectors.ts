f881cc9668b0880748a4bd2491efe568aded075c
Jeff Pihach <jeff.pihach@canonical.com>
2021-08-18 13:26:50 -0600

    Added selectors for the model applications and units.

--- src/juju/model-selectors.ts
+++ src/juju/model-selectors.ts
@@ -1,27 +1,41 @@
 import { createSelector } from "reselect";
 
 import type { ReduxState, ModelsList } from "types";
-import type { ModelInfo, ModelWatcherData } from "./types";
+import type {
+  ApplicationData,
+  ModelInfo,
+  ModelWatcherData,
+  UnitData,
+} from "./types";
 
 const getModelWatcherData = (state: ReduxState): ModelWatcherData =>
   state.juju.modelWatcherData;
 const getModelList = (state: ReduxState): ModelsList => state.juju.models;
 
-export function getModelInfoByUUID(modelUUID: string) {
+export function getModelWatcherDataByUUID(modelUUID: string) {
+  return createSelector(getModelWatcherData, (modelWatcherData) => {
+    if (modelWatcherData[modelUUID]) {
+      return modelWatcherData[modelUUID];
+    }
+    return null;
+  });
+}
+
+export function getModelInfo(modelUUID: string) {
   return createSelector(
-    getModelWatcherData,
+    getModelWatcherDataByUUID(modelUUID),
     (modelWatcherData): ModelInfo | null => {
-      if (modelWatcherData[modelUUID]) {
-        return modelWatcherData[modelUUID].model;
+      if (modelWatcherData) {
+        return modelWatcherData.model;
       }
       return null;
     }
   );
 }
 
-export function getModelUUIDByName(modelName: string, ownerName: string) {
+export function getModelUUID(modelName: string, ownerName: string) {
   return createSelector(getModelList, (modelList: ModelsList) => {
-    let modelUUID = null;
+    let modelUUID = "";
     Object.entries(modelList).some(([key, { name, ownerTag, uuid }]) => {
       if (name === modelName && ownerTag.replace("user-", "") === ownerName) {
         modelUUID = uuid;
@@ -31,4 +45,28 @@ export function getModelUUIDByName(modelName: string, ownerName: string) {
     });
     return modelUUID;
   });
+}
+
+export function getModelApplications(modelUUID: string) {
+  return createSelector(
+    getModelWatcherDataByUUID(modelUUID),
+    (modelWatcherData): ApplicationData | null => {
+      if (modelWatcherData) {
+        return modelWatcherData.applications;
+      }
+      return null;
+    }
+  );
+}
+
+export function getModelUnits(modelUUID: string) {
+  return createSelector(
+    getModelWatcherDataByUUID(modelUUID),
+    (modelWatcherData): UnitData | null => {
+      if (modelWatcherData) {
+        return modelWatcherData.units;
+      }
+      return null;
+    }
+  );
 }
