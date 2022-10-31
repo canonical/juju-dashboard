b72d996c6a3ddbdf1bd7c3872e6873f19bfd3cd6
Huw Wilkins <huw.wilkins@canonical.com>
2022-10-31 12:53:51 +1100

    Migrate UserMenu to RTL.

--- src/components/UserMenu/UserMenu.test.tsx
+++ src/components/UserMenu/UserMenu.test.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
-import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import cloneDeep from "clone-deep";
 import dataDump from "testing/complete-redux-store-dump";
 
@@ -11,21 +12,21 @@ const mockStore = configureStore([]);
 describe("User Menu", () => {
   it("is inactive by default", () => {
     const store = mockStore(dataDump);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <Router>
           <UserMenu />
         </Router>
       </Provider>
     );
-    expect(wrapper.find(".user-menu").hasClass("is-active")).toEqual(false);
+    expect(document.querySelector(".user-menu")).not.toHaveClass("is-active");
   });
 
   it("is active when userMenuActive in redux store is true", () => {
     const clonedDump = cloneDeep(dataDump);
     clonedDump.ui.userMenuActive = true;
     const store = mockStore(clonedDump);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <Router>
           <UserMenu />
@@ -33,32 +34,31 @@ describe("User Menu", () => {
       </Provider>
     );
 
-    expect(wrapper.find(".user-menu").hasClass("is-active")).toEqual(true);
+    expect(document.querySelector(".user-menu")).toHaveClass("is-active");
   });
 
   it("displays current logged in user", () => {
     const store = mockStore(dataDump);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <Router>
           <UserMenu />
         </Router>
       </Provider>
     );
-    const username = ".user-menu__name";
-    expect(wrapper.find(username).text()).toEqual("eggman");
+    expect(screen.getByText("eggman")).toHaveClass("user-menu__name");
   });
 
-  it("Test dispatch function is fired", () => {
+  it("Test dispatch function is fired", async () => {
     const store = mockStore(dataDump);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <Router>
           <UserMenu />
         </Router>
       </Provider>
     );
-    wrapper.find(".user-menu__header").simulate("click");
+    await userEvent.click(screen.getByRole("button"));
     const actions = store.getActions();
     const expectedPayload = { payload: true, type: "TOGGLE_USER_MENU" };
     expect(actions).toEqual([expectedPayload]);
