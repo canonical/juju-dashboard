7f41ed8b46406c8df080e60b7ce58c4841a61f72
huwshimi <huwshimi@users.noreply.github.com>
2022-12-07 09:52:40 +1100

    Migrate Banner to RTL. (#1294)

--- src/components/Banner/Banner.test.tsx
+++ src/components/Banner/Banner.test.tsx
@@ -1,65 +1,80 @@
-import { shallow, mount } from "enzyme";
-
-import Banner from "./Banner";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Banner, { Label } from "./Banner";
 
 describe("Banner", () => {
   it("should display banner text", () => {
-    const wrapper = shallow(
+    render(
       <Banner variant="positive" isActive={true}>
         <p>Banner text</p>
       </Banner>
     );
-    expect(wrapper.find("p").text()).toStrictEqual("Banner text");
+    expect(screen.getByText("Banner text")).toBeInTheDocument();
   });
 
   it("should appear if active", () => {
-    const wrapper = shallow(
-      <Banner variant="positive" isActive={true}>
+    render(
+      <Banner data-testid="banner" variant="positive" isActive={true}>
         <p>Banner text</p>
       </Banner>
     );
-    expect(wrapper.find("div").prop("data-active")).toStrictEqual(true);
+    expect(screen.getByTestId("banner")).toHaveAttribute("data-active", "true");
   });
 
   it("should not appear if not active", () => {
-    const wrapper = shallow(
-      <Banner variant="positive" isActive={false}>
+    render(
+      <Banner data-testid="banner" variant="positive" isActive={false}>
         <p>Banner text</p>
       </Banner>
     );
-    expect(wrapper.find("div").prop("data-active")).toStrictEqual(false);
+    expect(screen.getByTestId("banner")).toHaveAttribute(
+      "data-active",
+      "false"
+    );
   });
 
   it("should as cautionary if variant prop is set", () => {
-    const wrapper = shallow(
-      <Banner isActive={true} variant="caution">
+    render(
+      <Banner data-testid="banner" isActive={true} variant="caution">
         <p>Banner text</p>
       </Banner>
     );
-    expect(wrapper.find("div").prop("data-variant")).toStrictEqual("caution");
+    expect(screen.getByTestId("banner")).toHaveAttribute(
+      "data-variant",
+      "caution"
+    );
   });
 
-  it("should close if close button is pressed", () => {
-    const wrapper = shallow(
-      <Banner isActive={true} variant="caution">
+  it("should close if close button is pressed", async () => {
+    render(
+      <Banner data-testid="banner" isActive={true} variant="caution">
         <p>Banner text</p>
       </Banner>
     );
-    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(true);
-    wrapper.find(".banner__close").simulate("click");
-    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(false);
+    expect(screen.getByTestId("banner")).toHaveAttribute("data-active", "true");
+    await userEvent.click(screen.getByRole("button", { name: Label.CLOSE }));
+    expect(screen.getByTestId("banner")).toHaveAttribute(
+      "data-active",
+      "false"
+    );
   });
 
-  it("should return if variant changes after close button is pressed", () => {
-    const wrapper = mount(
-      <Banner isActive={true} variant="caution">
+  it("should return if variant changes after close button is pressed", async () => {
+    const { rerender } = render(
+      <Banner data-testid="banner" isActive={true} variant="caution">
         <p>Banner text</p>
       </Banner>
     );
-    wrapper.find(".banner__close").simulate("click");
-    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(false);
-    wrapper.setProps({ variant: "positive", isActive: true });
-    wrapper.update();
-    expect(wrapper.find(".banner").prop("data-active")).toStrictEqual(true);
+    await userEvent.click(screen.getByRole("button", { name: Label.CLOSE }));
+    expect(screen.getByTestId("banner")).toHaveAttribute(
+      "data-active",
+      "false"
+    );
+    rerender(
+      <Banner data-testid="banner" isActive={true} variant="positive">
+        <p>Banner text</p>
+      </Banner>
+    );
+    expect(screen.getByTestId("banner")).toHaveAttribute("data-active", "true");
   });
 });
