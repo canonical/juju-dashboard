a7d4c827e85ca7138731072986634e0fcdf6bee1
Huw Wilkins <huw.wilkins@canonical.com>
2023-03-15 15:36:06 +1100

    Revoke all permissions when removing a user from a model.

--- src/panels/ShareModelPanel/ShareModel.test.tsx
+++ src/panels/ShareModelPanel/ShareModel.test.tsx
@@ -5,13 +5,17 @@ import { Provider } from "react-redux";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
 import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
+import { actions as appActions } from "store/app";
 
+import { Label } from "components/ShareCard/ShareCard";
 import { RootState } from "store/store";
 import { rootStateFactory } from "testing/factories";
 import {
   jujuStateFactory,
   modelDataFactory,
   modelDataInfoFactory,
+  modelUserInfoFactory,
+  controllerFactory,
 } from "testing/factories/juju/juju";
 
 import ShareModel from "./ShareModel";
@@ -21,13 +25,23 @@ const mockStore = configureStore([]);
 describe("Share Model Panel", () => {
   let state: RootState;
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   beforeEach(() => {
     state = rootStateFactory.build({
       juju: jujuStateFactory.build({
+        controllers: {
+          "wss://jimm.jujucharms.com/api": [
+            controllerFactory.build({ path: "admin/jaas", uuid: "123" }),
+          ],
+        },
         modelData: {
           abc123: modelDataFactory.build({
             info: modelDataInfoFactory.build({
               name: "hadoopspark",
+              users: [modelUserInfoFactory.build()],
             }),
           }),
         },
@@ -81,5 +95,37 @@ describe("Share Model Panel", () => {
       name: "Back",
     });
     expect(backToCardsButton).toBeInTheDocument();
+  });
+
+  it("can remove a user", async () => {
+    const updatePermissionsSpy = jest.spyOn(appActions, "updatePermissions");
+    const store = mockStore(state);
+    render(
+      <MemoryRouter initialEntries={["/models/eggman@external/hadoopspark"]}>
+        <Provider store={store}>
+          <QueryParamProvider adapter={ReactRouter6Adapter}>
+            <Routes>
+              <Route
+                path="/models/:userName/:modelName"
+                element={<ShareModel />}
+              />
+            </Routes>
+          </QueryParamProvider>
+        </Provider>
+      </MemoryRouter>
+    );
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: Label.REMOVE,
+      })
+    );
+    expect(updatePermissionsSpy).toHaveBeenCalledWith({
+      action: "revoke",
+      modelUUID: "84e872ff-9171-46be-829b-70f0ffake18d",
+      permissionFrom: "read",
+      permissionTo: undefined,
+      user: "user-eggman@external",
+      wsControllerURL: "wss://jimm.jujucharms.com/api",
+    });
   });
 });
