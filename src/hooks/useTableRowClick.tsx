b7db35b7e175a62c81a83690e9bbfbf3daa58362
Huw Wilkins <huw.wilkins@canonical.com>
2023-03-28 15:39:41 +1100

    Use URL definitions for navigate() calls.

--- src/hooks/useTableRowClick.tsx
+++ src/hooks/useTableRowClick.tsx
@@ -1,5 +1,6 @@
 import { MouseEvent, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import urls from "urls";
 
 type Params = {
   userName: string;
@@ -18,20 +19,33 @@ export default function useTableRowClick() {
 
   useEffect(() => {
     const entityId = entity && entity.id.replace("/", "-");
+    if (!userName || !modelName || !entityId) {
+      return;
+    }
+    let url: string | null = null;
     if (entity?.type === "unit") {
       const appName = entityId?.split("-").slice(0, -1).join("-");
-      userName &&
-        modelName &&
-        entity &&
-        appName &&
-        navigate(
-          `/models/${userName}/${modelName}/app/${appName}/${entity.type}/${entityId}/`
-        );
-    } else {
-      userName &&
-        modelName &&
-        entity &&
-        navigate(`/models/${userName}/${modelName}/${entity.type}/${entityId}`);
+      url = urls.model.unit({
+        userName,
+        modelName,
+        appName,
+        unitId: entityId,
+      });
+    } else if (entity?.type === "machine") {
+      url = urls.model.machine({
+        userName,
+        modelName,
+        machineId: entityId,
+      });
+    } else if (entity?.type === "app") {
+      url = urls.model.app({
+        userName,
+        modelName,
+        appName: entityId,
+      });
+    }
+    if (url) {
+      navigate(url);
     }
   }, [entity, navigate, modelName, userName]);
 
