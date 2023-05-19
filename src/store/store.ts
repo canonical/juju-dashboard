3cfc341eb106b81a2721140e8730d74dfc27d8a9
Huw Wilkins <huw.wilkins@canonical.com>
2023-05-19 10:01:19 +1000

    Support preloading redux data.

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,6 +9,15 @@ import checkAuth from "store/middleware/check-auth";
 import { modelPollerMiddleware } from "store/middleware/model-poller";
 import uiReducer from "store/ui";
 
+let preloadedState: Record<string, unknown> | undefined;
+if (process.env.NODE_ENV !== "production" && process.env.REACT_APP_MOCK_STORE) {
+  try {
+    preloadedState = JSON.parse(process.env.REACT_APP_MOCK_STORE);
+  } catch (error) {
+    console.error("REACT_APP_MOCK_STORE could not be parsed");
+  }
+}
+
 const store = configureStore({
   // Order of the middleware is important
   middleware: (getDefaultMiddleware) => {
@@ -21,6 +30,7 @@ const store = configureStore({
     middleware.push(modelPollerMiddleware);
     return middleware;
   },
+  preloadedState,
   reducer: {
     general: generalReducer,
     juju: jujuReducer,
