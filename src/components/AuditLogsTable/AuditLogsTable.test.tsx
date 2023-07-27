ceb2695c9f31c39c9c08d77828610355b6036f5b
vladimir-cucu <vladimir.cucu@canonical.com>
2023-07-27 17:22:46 +0300

    feat: Hide model column from table in model logs view

--- src/components/AuditLogsTable/AuditLogsTable.test.tsx
+++ src/components/AuditLogsTable/AuditLogsTable.test.tsx
@@ -3,8 +3,18 @@ import { renderComponent } from "testing/utils";
 import AuditLogsTable from "./AuditLogsTable";
 
 describe("AuditLogsTable", () => {
-  it("should have table data", () => {
-    renderComponent(<AuditLogsTable />);
+  it("should have model as second header when showing the model data", () => {
+    renderComponent(<AuditLogsTable showModel={true} />);
     expect(document.querySelector("table tr")).toBeVisible();
+    expect(document.querySelectorAll("table th")[1]).toHaveTextContent("model");
+  });
+
+  it("should not have model as header when not showing the model data", () => {
+    renderComponent(<AuditLogsTable showModel={false} />);
+    expect(document.querySelector("table tr")).toBeVisible();
+    const headers = document.querySelectorAll("table th");
+    headers.forEach((header) => {
+      expect(header).not.toHaveTextContent("model");
+    });
   });
 });
