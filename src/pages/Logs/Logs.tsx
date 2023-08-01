a7f42ab45b389d5a0c0f779007519f99973b0f1c
Vladimir Cucu <108150922+vladimir-cucu@users.noreply.github.com>
2023-08-01 18:25:39 +0300

    WD-5211 - feat: Add Logs table to top level Logs page (#1547)
    * feat: Add Logs table to top level Logs page
    
    * fix: Fix and improve the scss for BaseLayout
    
    * fix: Make only the table overflow in Logs

--- src/pages/Logs/Logs.tsx
+++ src/pages/Logs/Logs.tsx
@@ -1,3 +1,5 @@
+import FadeIn from "animations/FadeIn";
+import AuditLogsTable from "components/AuditLogsTable/AuditLogsTable";
 import Header from "components/Header/Header";
 import BaseLayout from "layout/BaseLayout/BaseLayout";
 
@@ -6,6 +8,11 @@ const Logs = (): JSX.Element => (
     <Header>
       <b>Audit logs</b>
     </Header>
+    <div className="l-content logs u-overflow--auto">
+      <FadeIn isActive={true}>
+        <AuditLogsTable showModel />
+      </FadeIn>
+    </div>
   </BaseLayout>
 );
 
