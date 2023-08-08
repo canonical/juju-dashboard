c6b194686bb3f218de03c15ba56283129b5fd12e
Vladimir Cucu <108150922+vladimir-cucu@users.noreply.github.com>
2023-08-09 02:31:08 +0300

    refactor: Move Panel inside CharmsPanel and CharmActionsPanel (#1556)
    * refactor: Move Panel inside CharmsPanel and CharmActions Panel

--- src/panels/CharmsPanel/CharmsPanel.tsx
+++ src/panels/CharmsPanel/CharmsPanel.tsx
@@ -3,22 +3,48 @@ import type { FormEventHandler } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
+import Panel from "components/Panel";
+import { TestId } from "panels/CharmsAndActionsPanel/CharmsAndActionsPanel";
 import { getCharms } from "store/juju/selectors";
+
 import "./_charms-panel.scss";
+
+export enum Label {
+  PANEL_TITLE = "Choose applications of charm:",
+}
+
+export enum ClassName {
+  PANEL = "charms-panel",
+}
 
 type Props = {
   onCharmURLChange: (charmURL: string | null) => void;
+  onRemovePanelQueryParams: () => void;
+  isLoading: boolean;
 };
 
-export default function CharmsPanel({ onCharmURLChange }: Props): JSX.Element {
+export default function CharmsPanel({
+  onCharmURLChange,
+  isLoading,
+  onRemovePanelQueryParams,
+}: Props): JSX.Element {
   const [selectedCharm, setSelectedCharm] = useState<string | null>(null);
   const charms = useSelector(getCharms());
+
   const handleSubmit: FormEventHandler = (e) => {
     e.preventDefault();
     onCharmURLChange(selectedCharm);
   };
+
   return (
-    <>
+    <Panel
+      width="narrow"
+      panelClassName={ClassName.PANEL}
+      data-testid={TestId.PANEL}
+      title={Label.PANEL_TITLE}
+      onRemovePanelQueryParams={onRemovePanelQueryParams}
+      loading={isLoading}
+    >
       <div className="p-panel__content p-panel_content--padded">
         <form
           className="p-form u-fixed-width charm-list"
@@ -47,6 +73,6 @@ export default function CharmsPanel({ onCharmURLChange }: Props): JSX.Element {
           </div>
         </form>
       </div>
-    </>
+    </Panel>
   );
 }
