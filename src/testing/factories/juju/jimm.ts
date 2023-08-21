af0b118924451fa8914a92ebc8c27f6a1ccb4c2c
vladimir-cucu <vladimir.cucu@canonical.com>
2023-08-21 11:02:24 +0300

    fix: Resolve review comments

--- src/testing/factories/juju/jimm.ts
+++ src/testing/factories/juju/jimm.ts
@@ -2,171 +2,84 @@ import { Factory } from "fishery";
 
 import type {
   CrossModelQuery,
-  Machine,
-  Model,
-  Unit,
-} from "./CrossModelQuery.types";
+  CrossModelQueryApplication,
+  CrossModelQueryApplicationEndpoint,
+  CrossModelQueryMachine,
+  CrossModelQueryModel,
+  CrossModelQueryStatus,
+  CrossModelQueryUnit,
+} from "./jimm-types";
 
-export const crossModelQueryFactory = Factory.define<CrossModelQuery>(() => ({
-  "application-endpoints": {
+export const crossModelQueryFactory = Factory.define<CrossModelQuery>(
+  () => ({})
+);
+
+export const crossModelQueryApplicationEndpointFactory =
+  Factory.define<CrossModelQueryApplicationEndpoint>(() => ({
     "mysql-cmi": {
-      "application-status": {
-        current: "unknown",
-        since: "16 Aug 2023 10:47:30+10:00",
-      },
+      "application-status": crossModelQueryStatusFactory.build(),
       endpoints: { mysql: { interface: "mysql", role: "provider" } },
       relations: { mysql: ["slurmdbd"] },
       url: "jaas-staging:huwshimi@external/cmi-provider.mysql-cmi",
     },
-  },
-  applications: {
-    slurmdbd: {
-      "application-status": {
-        current: "unknown",
-        since: "16 Aug 2023 10:46:06+10:00",
-      },
-      base: { channel: "20.04", name: "ubuntu" },
-      charm: "slurmdbd",
-      "charm-channel": "stable",
-      "charm-name": "slurmdbd",
-      "charm-origin": "charmhub",
-      "charm-rev": 18,
-      "charm-version": "0.8.5",
-      "endpoint-bindings": {
-        "": "alpha",
-        db: "alpha",
-        fluentbit: "alpha",
-        "nrpe-external-master": "alpha",
-        slurmdbd: "alpha",
-        "slurmdbd-peer": "alpha",
-      },
-      exposed: false,
-      relations: { db: ["mysql-cmi"], "slurmdbd-peer": ["slurmdbd"] },
+  }));
+
+export const crossModelQueryApplicationFactory =
+  Factory.define<CrossModelQueryApplication>(() => ({
+    "application-status": crossModelQueryStatusFactory.build(),
+    base: { channel: "22.04", name: "ubuntu" },
+    charm: "calico",
+    "charm-channel": "stable",
+    "charm-name": "calico",
+    "charm-origin": "charmhub",
+    "charm-rev": 87,
+    "charm-version": "a164af4",
+    "endpoint-bindings": { "": "alpha", cni: "alpha", etcd: "alpha" },
+    exposed: false,
+    relations: {
+      cni: ["kubernetes-control-plane", "kubernetes-worker"],
+      etcd: ["etcd"],
     },
-  },
-  controller: { timestamp: "10:51:03+10:00" },
-  machines: {
-    "0": {
-      base: { channel: "22.04", name: "ubuntu" },
-      constraints: "cores=2 mem=8192M root-disk=16384M",
-      containers: {
-        "0/lxd/0": {
-          base: { channel: "22.04", name: "ubuntu" },
-          "instance-id": "pending",
-          "juju-status": {
-            current: "pending",
-            since: "16 Aug 2023 10:33:52+10:00",
-          },
-          "machine-status": {
-            current: "pending",
-            since: "16 Aug 2023 10:33:52+10:00",
-          },
-          "modification-status": {
-            current: "idle",
-            since: "16 Aug 2023 10:33:52+10:00",
-          },
-        },
-      },
-      "dns-name": "44.234.46.248",
-      hardware:
-        "arch=amd64 cores=2 cpu-power=700 mem=8192M root-disk=16384M availability-zone=us-west-2d",
-      "instance-id": "i-0786c85c882bf194e",
-      "ip-addresses": ["44.234.46.248", "172.31.60.224"],
-      "juju-status": {
-        current: "pending",
-        since: "16 Aug 2023 10:33:46+10:00",
-      },
-      "machine-status": {
-        current: "running",
-        message: "running",
-        since: "16 Aug 2023 10:34:09+10:00",
-      },
-      "modification-status": {
-        current: "idle",
-        since: "16 Aug 2023 10:33:46+10:00",
-      },
-    },
-  },
-  model: {
+    "subordinate-to": ["kubernetes-control-plane", "kubernetes-worker"],
+  }));
+
+export const crossModelQueryUnitFactory = Factory.define<CrossModelQueryUnit>(
+  () => ({
+    "juju-status": crossModelQueryStatusFactory.build(),
+    machine: "0/lxd/0",
+    "workload-status": crossModelQueryStatusFactory.build(),
+  })
+);
+
+export const crossModelQueryMachineFactory =
+  Factory.define<CrossModelQueryMachine>(() => ({
+    base: { channel: "22.04", name: "ubuntu" },
+    constraints: "cores=2 mem=8192M root-disk=16384M",
+    "dns-name": "44.234.46.248",
+    hardware:
+      "arch=amd64 cores=2 cpu-power=700 mem=8192M root-disk=16384M availability-zone=us-west-2d",
+    "instance-id": "i-0786c85c882bf194e",
+    "ip-addresses": ["44.234.46.248", "172.31.60.224"],
+    "juju-status": crossModelQueryStatusFactory.build(),
+    "machine-status": crossModelQueryStatusFactory.build(),
+    "modification-status": crossModelQueryStatusFactory.build(),
+  }));
+
+export const crossModelQueryModelFactory = Factory.define<CrossModelQueryModel>(
+  () => ({
     cloud: "aws",
     controller: "jaas-staging",
-    "model-status": {
-      current: "available",
-      since: "16 Aug 2023 10:45:46+10:00",
-    },
-    name: "cmi-consumer",
+    "model-status": crossModelQueryStatusFactory.build(),
+    name: "k8s2",
     region: "us-west-2",
     sla: "unsupported",
     type: "iaas",
     version: "3.2.2.1",
-  },
-  storage: {},
-}));
+  })
+);
 
-export const crossModelQueryUnitFactory = Factory.define<Unit>(() => ({
-  "juju-status": {
-    current: "allocating",
-    since: "16 Aug 2023 10:33:53+10:00",
-  },
-  machine: "0/lxd/0",
-  "workload-status": {
-    current: "waiting",
-    message: "waiting for machine",
-    since: "16 Aug 2023 10:33:53+10:00",
-  },
-}));
-
-export const crossModelQueryMachineFactory = Factory.define<Machine>(() => ({
-  base: { channel: "22.04", name: "ubuntu" },
-  constraints: "cores=2 mem=8192M root-disk=16384M",
-  containers: {
-    "0/lxd/0": {
-      base: { channel: "22.04", name: "ubuntu" },
-      "instance-id": "pending",
-      "juju-status": {
-        current: "pending",
-        since: "16 Aug 2023 10:33:52+10:00",
-      },
-      "machine-status": {
-        current: "pending",
-        since: "16 Aug 2023 10:33:52+10:00",
-      },
-      "modification-status": {
-        current: "idle",
-        since: "16 Aug 2023 10:33:52+10:00",
-      },
-    },
-  },
-  "dns-name": "44.234.46.248",
-  hardware:
-    "arch=amd64 cores=2 cpu-power=700 mem=8192M root-disk=16384M availability-zone=us-west-2d",
-  "instance-id": "i-0786c85c882bf194e",
-  "ip-addresses": ["44.234.46.248", "172.31.60.224"],
-  "juju-status": {
+export const crossModelQueryStatusFactory =
+  Factory.define<CrossModelQueryStatus>(() => ({
     current: "pending",
     since: "16 Aug 2023 10:33:46+10:00",
-  },
-  "machine-status": {
-    current: "running",
-    message: "running",
-    since: "16 Aug 2023 10:34:09+10:00",
-  },
-  "modification-status": {
-    current: "idle",
-    since: "16 Aug 2023 10:33:46+10:00",
-  },
-}));
-
-export const crossModelQueryModelFactory = Factory.define<Model>(() => ({
-  cloud: "aws",
-  controller: "jaas-staging",
-  "model-status": {
-    current: "available",
-    since: "16 Aug 2023 10:32:17+10:00",
-  },
-  name: "k8s2",
-  region: "us-west-2",
-  sla: "unsupported",
-  type: "iaas",
-  version: "3.2.2.1",
-}));
+  }));
