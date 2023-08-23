064c70d2f196665da961f96c95eec2d377f96c56
Vladimir Cucu <108150922+vladimir-cucu@users.noreply.github.com>
2023-08-23 08:13:35 +0300

    WD-5816 WD-5817 - feat: Add results block with mode switcher (#1580)
    * feat: Add results block with mode switcher
    
    
    ---------
    
    Co-authored-by: Huw Wilkins <huw.wilkins@canonical.com>

---
+++ src/pages/AdvancedSearch/ResultsBlock/ResultsBlock.tsx
@@ -0,0 +1,99 @@
+import {
+  CodeSnippet,
+  CodeSnippetBlockAppearance,
+  Spinner,
+} from "@canonical/react-components";
+import { useEffect, useState } from "react";
+
+import { actions as jujuActions } from "store/juju";
+import {
+  getCrossModelQueryLoaded,
+  getCrossModelQueryLoading,
+  getCrossModelQueryResults,
+} from "store/juju/selectors";
+import { useAppDispatch, useAppSelector } from "store/store";
+
+export enum TestId {
+  LOADING = "loading",
+}
+
+enum CodeSnippetView {
+  TREE = "tree",
+  JSON = "json",
+}
+
+const ResultsBlock = (): JSX.Element | null => {
+  const dispatch = useAppDispatch();
+  const isCrossModelQueryLoading = useAppSelector(getCrossModelQueryLoading);
+  const isCrossModelQueryLoaded = useAppSelector(getCrossModelQueryLoaded);
+  const crossModelQueryResults = useAppSelector(getCrossModelQueryResults);
+  const [codeSnippetView, setCodeSnippetView] = useState<CodeSnippetView>(
+    CodeSnippetView.TREE
+  );
+  const codeSnippetContent = {
+    tree: "",
+    json: JSON.stringify(crossModelQueryResults, null, 2),
+  };
+
+  useEffect(
+    () => () => {
+      // Clear cross-model query results when component is unmounted.
+      dispatch(jujuActions.clearCrossModelQuery());
+    },
+    [dispatch]
+  );
+
+  if (!isCrossModelQueryLoaded) {
+    return null;
+  }
+
+  if (isCrossModelQueryLoading) {
+    return (
+      <div className="u-align--center">
+        <div className="u-sv3">
+          <hr />
+        </div>
+        <Spinner data-testid={TestId.LOADING} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <hr />
+      <CodeSnippet
+        blocks={[
+          {
+            appearance:
+              codeSnippetView === CodeSnippetView.JSON
+                ? CodeSnippetBlockAppearance.NUMBERED
+                : undefined,
+            code: codeSnippetContent[codeSnippetView],
+            dropdowns: [
+              {
+                options: [
+                  {
+                    value: CodeSnippetView.TREE,
+                    label: "Tree",
+                  },
+                  {
+                    value: CodeSnippetView.JSON,
+                    label: "JSON",
+                  },
+                ],
+                value: codeSnippetView,
+                onChange: (event) => {
+                  setCodeSnippetView(
+                    (event.target as HTMLSelectElement).value as CodeSnippetView
+                  );
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+export default ResultsBlock;
