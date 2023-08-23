225061051077172a53965d1ed4a72aa1231b3211
huwshimi <huwshimi@users.noreply.github.com>
2023-08-24 09:31:19 +1000

    WD-5814 - Add help (#1584)
    * Add cross model queries help.

---
+++ src/pages/AdvancedSearch/SearchForm/SearchHelp/SearchHelp.tsx
@@ -0,0 +1,61 @@
+import { useFormikContext } from "formik";
+
+import QueryLink from "../QueryLink";
+import type { FormFields } from "../types";
+
+type Props = {
+  search: (query: string) => void;
+};
+
+const SearchHelp = ({ search }: Props): JSX.Element => {
+  const { setFieldValue } = useFormikContext<FormFields>();
+
+  const handleQuery = (query: string) => {
+    setFieldValue("query", query);
+    search(query);
+  };
+
+  return (
+    <>
+      <p className="p-form-help-text u-no-margin--top">
+        Search all models using the{" "}
+        <a
+          href="https://jqlang.github.io/jq/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          JQ
+        </a>{" "}
+        syntax. To see the JSON that is queried you can use the CLI to run{" "}
+        <code>juju status --format json</code> or search for{" "}
+        <QueryLink query="." handleQuery={handleQuery} /> to return the
+        unmodified output for all models.
+      </p>
+      <div className="u-hide u-show--large">
+        <p className="p-form-help-text">Examples:</p>
+        <ul className="p-form-help-text">
+          <li>
+            Get all applications:{" "}
+            <QueryLink query=".applications" handleQuery={handleQuery} />
+          </li>
+          <li>
+            Get all available models:{" "}
+            <QueryLink
+              query={`. | select(."model-status".current=="available")`}
+              handleQuery={handleQuery}
+            />
+          </li>
+          <li>
+            Get all models that contain an application named postgresql:{" "}
+            <QueryLink
+              query={`select(.applications | to_entries[] | select(.key=="postgresql")) | .`}
+              handleQuery={handleQuery}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default SearchHelp;
--- src/pages/AdvancedSearch/SearchForm/SearchHistoryMenu/SearchHistoryMenu.test.tsx
+++ src/pages/AdvancedSearch/SearchForm/SearchHistoryMenu/SearchHistoryMenu.test.tsx
@@ -13,14 +13,18 @@ describe("SearchHistoryMenu", () => {
   it("should be disabled when there is no history", async () => {
     renderComponent(
       <Formik initialValues={{ query: "" }} onSubmit={jest.fn()}>
-        <SearchHistoryMenu queryHistory={[]} setQueryHistory={jest.fn()} />
+        <SearchHistoryMenu
+          queryHistory={[]}
+          search={jest.fn()}
+          setQueryHistory={jest.fn()}
+        />
       </Formik>,
       { url: "/?q=.applications" }
     );
     expect(screen.getByRole("button", { name: Label.HISTORY })).toBeDisabled();
   });
 
-  it("should be disabled while loading", async () => {
+  it("should disable the items while loading", async () => {
     const state = rootStateFactory.build({
       juju: jujuStateFactory.build({
         crossModelQuery: crossModelQueryStateFactory.build({
@@ -30,11 +34,19 @@ describe("SearchHistoryMenu", () => {
     });
     renderComponent(
       <Formik initialValues={{ query: "" }} onSubmit={jest.fn()}>
-        <SearchHistoryMenu queryHistory={[]} setQueryHistory={jest.fn()} />
+        <SearchHistoryMenu
+          queryHistory={[".applications"]}
+          search={jest.fn()}
+          setQueryHistory={jest.fn()}
+        />
       </Formik>,
       { state, url: "/?q=.applications" }
     );
-    expect(screen.getByRole("button", { name: Label.HISTORY })).toBeDisabled();
+    // Open the menu so that the portal gets rendered.
+    await userEvent.click(screen.getByRole("button", { name: Label.HISTORY }));
+    expect(
+      screen.getByRole("button", { name: ".applications" })
+    ).toBeDisabled();
   });
 
   it("should display the history in the menu", async () => {
@@ -42,6 +54,7 @@ describe("SearchHistoryMenu", () => {
       <Formik initialValues={{ query: "" }} onSubmit={jest.fn()}>
         <SearchHistoryMenu
           queryHistory={[".applications", ".machines"]}
+          search={jest.fn()}
           setQueryHistory={jest.fn()}
         />
       </Formik>,
@@ -57,13 +70,15 @@ describe("SearchHistoryMenu", () => {
     ).toBeInTheDocument();
   });
 
-  it("should update the field and query param when clicking an item", async () => {
+  it("should update the field and perform a search when clicking an item", async () => {
+    const search = jest.fn();
     renderComponent(
       <Formik initialValues={{ query: "" }} onSubmit={jest.fn()}>
         <Form>
           <Field name="query" />
           <SearchHistoryMenu
             queryHistory={[".applications", ".machines"]}
+            search={search}
             setQueryHistory={jest.fn()}
           />
         </Form>
@@ -73,7 +88,7 @@ describe("SearchHistoryMenu", () => {
     // Open the menu so that the portal gets rendered.
     await userEvent.click(screen.getByRole("button", { name: Label.HISTORY }));
     await userEvent.click(screen.getByRole("button", { name: ".machines" }));
-    expect(window.location.search).toBe("?q=.machines");
+    expect(search).toHaveBeenCalledWith(".machines");
     expect(screen.getByRole("textbox")).toHaveValue(".machines");
   });
 
@@ -83,6 +98,7 @@ describe("SearchHistoryMenu", () => {
       <Formik initialValues={{ query: "" }} onSubmit={jest.fn()}>
         <SearchHistoryMenu
           queryHistory={[".applications", ".machines"]}
+          search={jest.fn()}
           setQueryHistory={setQueryHistory}
         />
       </Formik>,
