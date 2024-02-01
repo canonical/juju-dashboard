d29ecb5c9111c300c37193a2cba6105ca783fa17
vladimir-cucu <vladimir.cucu@canonical.com>
2024-02-01 21:13:53 +0200

    test: Add PanelInlineErrors tests

--- src/panels/PanelInlineErrors/PanelInlineErrors.test.tsx
+++ src/panels/PanelInlineErrors/PanelInlineErrors.test.tsx
@@ -12,12 +12,38 @@ describe("PanelInlineErrors", () => {
   });
 
   it("should render inline errors", () => {
-    render(<PanelInlineErrors inlineErrors={["Error 1", "Error 2"]} />);
+    render(
+      <PanelInlineErrors
+        inlineErrors={["Error 1", <button>Error 2</button>]}
+      />,
+    );
     const notifications = document.getElementsByClassName(
       "p-notification__message",
     );
     expect(notifications).toHaveLength(2);
     expect(notifications[0]).toHaveTextContent(/Error 1/);
     expect(notifications[1]).toHaveTextContent(/Error 2/);
+  });
+
+  it("shouldn't scroll on render in there is no inline error", () => {
+    render(
+      <PanelInlineErrors
+        inlineErrors={[null]}
+        scrollArea={document.createElement("div")}
+      />,
+    );
+    expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();
+  });
+
+  it("should scroll on render if there are inline errors", () => {
+    render(
+      <PanelInlineErrors
+        inlineErrors={[<button>Error</button>]}
+        scrollArea={document.createElement("div")}
+      />,
+    );
+    expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledWith({
+      behavior: "smooth",
+    });
   });
 });
