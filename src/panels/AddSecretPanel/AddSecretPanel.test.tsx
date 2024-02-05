c4946e78961e0aa68ac261fca78be06fa4c80a91
huwshimi <huwshimi@users.noreply.github.com>
2024-02-06 09:14:33 +1100

    WD-8546 - Create secrets (#1693)
    * Create secrets when the add form is submitted.

--- src/panels/AddSecretPanel/AddSecretPanel.test.tsx
+++ src/panels/AddSecretPanel/AddSecretPanel.test.tsx
@@ -1,12 +1,224 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+import * as apiHooks from "juju/apiHooks";
 import { renderComponent } from "testing/utils";
 
-import AddSecretPanel, { TestId } from "./AddSecretPanel";
+import AddSecretPanel, { Label } from "./AddSecretPanel";
+import { Label as FieldsLabel } from "./Fields/Fields";
+import { RotatePolicy } from "./types";
+
+jest.mock("juju/apiHooks", () => {
+  return {
+    useCreateSecrets: jest.fn().mockReturnValue(jest.fn()),
+  };
+});
 
 describe("AddSecretPanel", () => {
-  it("renders", async () => {
+  it("can create a secret", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ results: [] }));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
     renderComponent(<AddSecretPanel />);
-    expect(screen.getByTestId(TestId.PANEL)).toBeInTheDocument();
+    await userEvent.type(
+      screen.getByRole("textbox", { name: FieldsLabel.LABEL }),
+      "a label",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: FieldsLabel.DESCRIPTION }),
+      "a description",
+    );
+    const expiryField = document.querySelector('input[name="expiryTime"]');
+    expect(expiryField).toBeInTheDocument();
+    const expiry = "2024-02-17T13:14";
+    if (expiryField) {
+      await userEvent.type(expiryField, expiry);
+    }
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: FieldsLabel.ROTATE_POLICY }),
+      RotatePolicy.MONTHLY,
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(createSecrets).toHaveBeenCalledWith([
+      {
+        content: {
+          data: {
+            "a key": "YSB2YWx1ZQ==",
+          },
+        },
+        description: "a description",
+        "expire-time": new Date(expiry).toISOString(),
+        label: "a label",
+        "rotate-policy": "monthly",
+      },
+    ]);
+  });
+
+  it("encodes unencoded values", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ results: [] }));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />);
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(createSecrets).toHaveBeenCalledWith([
+      {
+        content: {
+          data: {
+            "a key": "YSB2YWx1ZQ==",
+          },
+        },
+        description: "",
+        "expire-time": undefined,
+        label: undefined,
+        "rotate-policy": "never",
+      },
+    ]);
+  });
+
+  it("does not encode encoded values", async () => {
+    const encoded = "YSB2YWx1ZQ==";
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ results: [] }));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />);
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      encoded,
+    );
+    await userEvent.click(
+      screen.getByRole("checkbox", { name: FieldsLabel.IS_BASE_64 }),
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(createSecrets).toHaveBeenCalledWith([
+      {
+        content: {
+          data: {
+            "a key": encoded,
+          },
+        },
+        description: "",
+        "expire-time": undefined,
+        label: undefined,
+        "rotate-policy": "never",
+      },
+    ]);
+  });
+
+  it("displays caught errors", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.reject(new Error("Caught error")));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />);
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(
+      document.querySelector(".p-notification--negative"),
+    ).toHaveTextContent("Caught error");
+  });
+
+  it("displays error string results", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve("String error"));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />);
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(
+      document.querySelector(".p-notification--negative"),
+    ).toHaveTextContent("String error");
+  });
+
+  it("displays error object results", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() =>
+        Promise.resolve({ results: [{ error: { message: "Error result" } }] }),
+      );
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />);
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(
+      document.querySelector(".p-notification--negative"),
+    ).toHaveTextContent("Error result");
+  });
+
+  it("closes the panel if successful", async () => {
+    const createSecrets = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ results: [] }));
+    jest
+      .spyOn(apiHooks, "useCreateSecrets")
+      .mockImplementation(() => createSecrets);
+    renderComponent(<AddSecretPanel />, { url: "?panel=add-secret" });
+    expect(window.location.search).toEqual("?panel=add-secret");
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.KEY} 1` }),
+      "a key",
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: `${FieldsLabel.VALUE} 1` }),
+      "a value",
+    );
+    await userEvent.click(screen.getByRole("button", { name: Label.SUBMIT }));
+    expect(window.location.search).toEqual("");
   });
 });
--- src/panels/AddSecretPanel/AddSecretPanel.tsx
+++ src/panels/AddSecretPanel/AddSecretPanel.tsx
@@ -1,13 +1,17 @@
-import {
-  ActionButton,
-  Button,
-  Notification,
-} from "@canonical/react-components";
+import type {
+  CreateSecretArg,
+  StringResults,
+} from "@canonical/jujulib/dist/api/facades/secrets/SecretsV2";
+import { ActionButton, Button } from "@canonical/react-components";
 import { Form, Formik } from "formik";
 import { useId, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import * as Yup from "yup";
 
 import Panel from "components/Panel";
-import ScrollOnRender from "components/ScrollOnRender";
+import type { EntityDetailsRoute } from "components/Routes/Routes";
+import { useCreateSecrets } from "juju/apiHooks";
+import PanelInlineErrors from "panels/PanelInlineErrors";
 import { usePanelQueryParams } from "panels/hooks";
 
 import Fields from "./Fields";
@@ -22,7 +26,26 @@ export enum Label {
   SUBMIT = "Add secret",
 }
 
+const schema = Yup.object().shape({
+  pairs: Yup.array()
+    .of(
+      Yup.object().shape({
+        key: Yup.string()
+          .required("Required")
+          .test(
+            "is-file",
+            "File uploads not supported.",
+            (value) => !value.endsWith("#file"),
+          ),
+        value: Yup.string().required("Required"),
+      }),
+    )
+    .required("Must have a key value pair.")
+    .min(1, "Minimum of 1 pair."),
+});
+
 const AddSecretPanel = () => {
+  const { userName, modelName } = useParams<EntityDetailsRoute>();
   const scrollArea = useRef<HTMLDivElement>(null);
   const formId = useId();
   const [, , handleRemovePanelQueryParams] = usePanelQueryParams<{
@@ -30,15 +53,11 @@ const AddSecretPanel = () => {
   }>({
     panel: null,
   });
-  const [inlineError, setInlineError] = useState<string[] | null>(null);
+  const [inlineError, setInlineError] = useState<string | null>(null);
   const [saving, setSaving] = useState<boolean>(false);
+  const createSecrets = useCreateSecrets(userName, modelName);
   return (
     <>
-      {inlineError ? (
-        <ScrollOnRender scrollArea={scrollArea?.current}>
-          <Notification severity="negative">{inlineError}</Notification>
-        </ScrollOnRender>
-      ) : null}
       <Panel
         data-testid={TestId.PANEL}
         drawer={
@@ -62,25 +81,80 @@ const AddSecretPanel = () => {
         title="Add a secret"
         width="narrow"
       >
-        <Formik<FormFields>
-          initialValues={{
-            content: "",
-            description: "",
-            expiryTime: "",
-            isBase64: false,
-            label: "",
-            rotatePolicy: RotatePolicy.NEVER,
-          }}
-          onSubmit={(values) => {
-            setSaving(true);
-            setInlineError(null);
-            handleRemovePanelQueryParams();
-          }}
-        >
-          <Form id={formId}>
-            <Fields />
-          </Form>
-        </Formik>
+        <>
+          <PanelInlineErrors
+            inlineErrors={[inlineError]}
+            scrollArea={scrollArea.current}
+          />
+          <Formik<FormFields>
+            initialValues={{
+              description: "",
+              expiryTime: "",
+              label: "",
+              pairs: [{ key: "", value: "", isBase64: false }],
+              rotatePolicy: RotatePolicy.NEVER,
+            }}
+            onSubmit={(values) => {
+              setSaving(true);
+              setInlineError(null);
+              const content = values.pairs.reduce<Record<string, string>>(
+                (contents, pair) => {
+                  // Encode the value unless the user has provided a string
+                  // that is already encoded.
+                  contents[pair.key] = pair.isBase64
+                    ? pair.value
+                    : btoa(pair.value);
+                  return contents;
+                },
+                {},
+              );
+              createSecrets([
+                {
+                  content: {
+                    data: content,
+                  },
+                  description: values.description,
+                  "expire-time": values.expiryTime
+                    ? new Date(values.expiryTime).toISOString()
+                    : undefined,
+                  "rotate-policy": values.rotatePolicy,
+                  label: values.label || undefined,
+                  // The type includes an UpsertSecretArg object due to a
+                  // schema.json issue which should be rectified in the future,
+                  // it also lists owner-tag as required though it is not.
+                } as CreateSecretArg,
+              ])
+                .then((response: StringResults | string) => {
+                  let error;
+                  if (typeof response === "string") {
+                    error = response;
+                  } else if (Array.isArray(response.results)) {
+                    error = response.results[0]?.error?.message;
+                  }
+                  if (error) {
+                    setSaving(false);
+                    setInlineError(error);
+                    return;
+                  }
+                  handleRemovePanelQueryParams();
+                  return;
+                })
+                .catch((error) => {
+                  setSaving(false);
+                  if (typeof error === "string" || error instanceof Error) {
+                    setInlineError(
+                      error instanceof Error ? error.message : error,
+                    );
+                  }
+                });
+            }}
+            validationSchema={schema}
+          >
+            <Form id={formId}>
+              <Fields />
+            </Form>
+          </Formik>
+        </>
       </Panel>
     </>
   );
