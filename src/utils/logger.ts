ad8d390f848e2524e72754de3d461fc507fba7dd
Urvashi Sharma <urvashi.sharma@canonical.com>
2025-02-07 11:00:24 +0530

    feat(logging): Attach logger to window

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,11 @@
 import log from "loglevel";
 
+declare global {
+  interface Window {
+    logger: log.Logger;
+  }
+}
+
+window.logger = log.getLogger("JujuDashboard");
+
 export const logger = log.getLogger("JujuDashboard");
