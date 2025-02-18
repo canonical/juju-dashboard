d304ffed7ae91d9aea1f0d0385306632a5d7a055
Huw Wilkins <huw.wilkins@canonical.com>
2025-02-18 12:52:44 +1100

    refactor: only fetch relations if they change and clean up the previous one

--- src/pages/EntityDetails/Model/Model.tsx
+++ src/pages/EntityDetails/Model/Model.tsx
@@ -87,11 +87,7 @@ const Model = () => {
   const canListSecrets = useAppSelector((state) =>
     getCanListSecrets(state, modelUUID),
   );
-  // Cleanup is set for this hook, but not for the instances of
-  // useCanConfigureModel in other model components as this component wraps all
-  // model routes so the model permissions are removed once the user navigates
-  // away from the model.
-  const canConfigureModel = useCanConfigureModel(true);
+  const canConfigureModel = useCanConfigureModel();
 
   const machinesTableRows = useMemo(() => {
     return modelName && userName
