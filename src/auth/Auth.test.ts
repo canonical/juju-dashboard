4b4e3f7e4f9b3e18d2594ae2e4eff49873f19b25
Tom Anderson <tom@ando.sh>
2025-03-27 15:17:56 +1100

    feat: implement base `Auth` class

---
+++ src/auth/Auth.test.ts
@@ -0,0 +1,32 @@
+import { AuthMethod } from "store/general/types";
+
+import { Auth } from "./Auth";
+
+describe("Auth base class", () => {
+  beforeEach(() => {
+    // @ts-expect-error - Resetting singleton for each test run.
+    delete Auth.instance;
+  });
+
+  it("creates new singleton instance", () => {
+    expect(Auth.instance).not.toBeDefined();
+    const instance = new Auth(vi.fn(), AuthMethod.LOCAL);
+    expect(Auth.instance).to.equal(instance);
+  });
+
+  it("overrides singleton instance", () => {
+    const instance1 = new Auth(vi.fn(), AuthMethod.LOCAL);
+    const instance2 = new Auth(vi.fn(), AuthMethod.LOCAL);
+    expect(Auth.instance).not.to.equal(instance1);
+    expect(Auth.instance).to.equal(instance2);
+  });
+
+  it("continues controller connection by default", async () => {
+    const instance = new Auth(vi.fn(), AuthMethod.LOCAL);
+    await expect(
+      instance.beforeControllerConnect({
+        wsControllerURL: "wss://1.2.3.4/api",
+      }),
+    ).resolves.to.equal(true);
+  });
+});
