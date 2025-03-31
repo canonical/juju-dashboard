16c4fd65e6b0e018916c2c7b0f3ff904bd7843da
Tom Anderson <tom@ando.sh>
2025-03-31 12:54:21 +1100

    feat: implement `LocalAuth` subclass
    WD-20707

---
+++ src/auth/LocalAuth.ts
@@ -0,0 +1,24 @@
+import type { Credentials } from "@canonical/jujulib";
+
+import type { Credential } from "store/general/types";
+import { AuthMethod } from "store/general/types";
+import type { AppDispatch } from "store/store";
+
+import { Auth } from "./Auth";
+
+export class LocalAuth extends Auth {
+  constructor(dispatch: AppDispatch) {
+    super(dispatch, AuthMethod.LOCAL);
+  }
+
+  determineCredentials(credential?: Credential): Credentials | undefined {
+    if (!credential) {
+      return;
+    }
+
+    return {
+      username: credential.user,
+      password: credential.password,
+    };
+  }
+}
