e6bced87963d0b0a2145878f5bf7b2d50e42cd16
Huw Wilkins <huw.wilkins@canonical.com>
2025-04-15 12:29:06 +1000

    WIP

--- jimm-login/test.spec.ts
+++ jimm-login/test.spec.ts
@@ -8,12 +8,19 @@ test("dashboard loads", async ({ page }) => {
   page.on("response", (response) => {
     console.log("response url: ", response.url());
   });
-  await page.goto("/");
-  await page.getByRole("link", { name: "Log in to the dashboard" }).click();
+  await page.goto("/iam-hydra/oauth2/device/verify");
+  console.log("Enter code:", process.env.DEVICE_CODE);
+  if (!process.env.DEVICE_CODE) {
+    test.fail(true, "Did not receive device code");
+  }
+  await page
+    .getByRole("textbox", { name: "XXXXXXXX" })
+    .fill(process.env.DEVICE_CODE as string);
+  await page.getByRole("button", { name: "Next" }).click();
   await page.getByRole("textbox", { name: "Email" }).fill("test@example.com");
   await page.getByRole("textbox", { name: /Password/ }).fill("test");
   await page.getByRole("button", { name: "Sign in" }).click();
-  await expect(page.getByText("1 model")).toBeVisible({
+  await expect(page.getByText("Sign in successful")).toBeVisible({
     timeout: 30_000,
   });
 });
