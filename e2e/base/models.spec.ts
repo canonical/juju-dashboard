df34403c9c0ae8f9a76fa5313a7b05fb5fb46a72
Urvashi Sharma <55801560+Ninfa-Jeon@users.noreply.github.com>
2025-04-17 09:54:47 +0530

    feat(e2e): Add test suite for Juju Web CLI (#1938)

--- e2e/base/models.spec.ts
+++ e2e/base/models.spec.ts
@@ -2,28 +2,30 @@ import { expect } from "@playwright/test";
 
 import { test } from "../fixtures/setup";
 
-test.beforeAll(async ({ jujuHelpers }) => {
-  await jujuHelpers.jujuLogin();
-  await jujuHelpers.addModel("foo");
-  await jujuHelpers.addSharedModel("bar", "John-Doe");
-  await jujuHelpers.adminLogin();
-});
+test.describe("Models", () => {
+  test.beforeAll(async ({ jujuHelpers }) => {
+    await jujuHelpers.jujuLogin();
+    await jujuHelpers.addModel("foo");
+    await jujuHelpers.addSharedModel("bar", "John-Doe");
+    await jujuHelpers.adminLogin();
+  });
 
-test.afterAll(async ({ jujuHelpers }) => {
-  await jujuHelpers.cleanup();
-});
+  test.afterAll(async ({ jujuHelpers }) => {
+    await jujuHelpers.cleanup();
+  });
 
-test("List created and shared models", async ({ page, authHelpers }) => {
-  await authHelpers.login();
+  test("List created and shared models", async ({ page, authHelpers }) => {
+    await authHelpers.login();
 
-  await expect(
-    page
-      .locator("tr", { hasText: "foo" })
-      .and(page.locator("tr", { hasText: "admin" })),
-  ).toBeInViewport();
-  await expect(
-    page
-      .locator("tr", { hasText: "bar" })
-      .and(page.locator("tr", { hasText: "John-Doe" })),
-  ).toBeInViewport();
+    await expect(
+      page
+        .locator("tr", { hasText: "foo" })
+        .and(page.locator("tr", { hasText: "admin" })),
+    ).toBeInViewport();
+    await expect(
+      page
+        .locator("tr", { hasText: "bar" })
+        .and(page.locator("tr", { hasText: "John-Doe" })),
+    ).toBeInViewport();
+  });
 });
