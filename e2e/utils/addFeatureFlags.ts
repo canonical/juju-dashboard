118d460f9d49eacf33f18e4164686b71453d6697
Huw Wilkins <huw.wilkins@canonical.com>
2025-05-15 10:03:09 +1000

    refactor: use URLSearchParams to append feature flags

--- e2e/utils/addFeatureFlags.ts
+++ e2e/utils/addFeatureFlags.ts
@@ -1,5 +1,11 @@
+const flags = [["enable-flag", "rebac"]];
+
 /**
  * Add required feature flags to a URL.
  */
-export const addFeatureFlags = (url: string): string =>
-  [url, "enable-flag=rebac"].join(url.includes("?") ? "&" : "?");
+export const addFeatureFlags = (url: string): string => {
+  const [path, search] = url.split("?");
+  const params = new URLSearchParams(search);
+  flags.forEach(([flag, value]) => params.append(flag, value));
+  return [path, params].join("?");
+};
