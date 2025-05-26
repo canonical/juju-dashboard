666cc4af7fd77aea02bf424b4f1f35b3a8f5e0bd
Huw Wilkins <huw.wilkins@canonical.com>
2025-05-26 15:20:42 +1000

    fix: small visual glitches in web CLI

---
+++ src/components/WebCLI/Output/consts.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_HEIGHT = 300;
+// 20 is a magic number, sometimes the browser stops firing the drag at
+// an inopportune time and the element isn't left completely closed.
+export const CONSIDER_CLOSED = 20;
+export const HELP_HEIGHT = 40;
