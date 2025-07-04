042967e6ea46b3068e64ffbd251f6805572206fd
Urvashi Sharma <urvashi.sharma@canonical.com>
2025-07-04 13:19:01 +0530

    chore: bump stylelint to fix lint errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import fs from "node:fs/promises";
+
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import fs from "node:fs/promises";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 /// <reference types="vitest" />
 export default defineConfig(({ mode }) => {
