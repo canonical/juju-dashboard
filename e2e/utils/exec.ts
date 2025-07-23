27a478a466fe5ae767a028681255cac0f973e564
Tom Anderson <tom.anderson@canonical.com>
2025-07-23 17:21:26 +1000

    exec error handling

--- e2e/utils/exec.ts
+++ e2e/utils/exec.ts
@@ -9,13 +9,23 @@ export function exec(command: string, ...args: string[]) {
   process.stdout.setEncoding("utf8");
   process.stderr.setEncoding("utf8");
 
-  const exit = new Promise((resolve, reject) => {
+  const exit = new Promise<void>((resolve, reject) => {
     process.on("close", (code, signal) => {
-      if (code !== null) {
-        return resolve(code);
+      if (code === 0) {
+        resolve();
+      } else if (code !== null) {
+        return reject(
+          new Error(
+            `Exit with non-zero status code (${code}): ${command} ${args.join(" ")}`,
+          ),
+        );
+      } else {
+        reject(
+          new Error(
+            `Process terminated with signal (${signal}): ${command} ${args.join(" ")}`,
+          ),
+        );
       }
-
-      reject(signal);
     });
   });
 
