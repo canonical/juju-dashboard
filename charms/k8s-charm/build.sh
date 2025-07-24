932322caaf6aab571192210d77d3a3f2a314e412
Urvashi Sharma <urvashi.sharma@canonical.com>
2025-07-24 10:52:57 +0530

    fix: discard output of microk8s step from build

--- charms/k8s-charm/build.sh
+++ charms/k8s-charm/build.sh
@@ -20,7 +20,7 @@ if [ "$BUILD_TYPE" == "checkout" ]; then
 
     # Attempt to import image into microk8s if available
     if command -v microk8s &> /dev/null; then
-        docker image save juju-dashboard:local | sudo microk8s ctr image import -
+        docker image save juju-dashboard:local | sudo microk8s ctr image import - >/dev/null
     fi
     DASHBOARD_IMAGE_ID="juju-dashboard:local"
 
