a5ed8781731c7b237390a598cb149b036344c237
Tom Anderson <tom@ando.sh>
2025-08-01 15:37:49 +1000

    feat: script to generate charmhub token for CI (#2016)
    * feat: script to generate charmhub token for CI
    
    * use full view permission
    
    * set 28 day ttl

---
+++ charms/generate-charmhub-token.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is to generate a token for CharmHub for use in CI. By default, the token will expire
+# after 30 days.
+# See: https://canonical-charmcraft.readthedocs-hosted.com/stable/howto/manage-the-current-charmhub-user/#log-in-to-charmhub
+
+echo 'Generating charmhub token to `charmhub-token.auth`'
+
+charmcraft login \
+    --export charmhub-token.auth \
+    --charm=juju-dashboard \
+    --charm=juju-dashboard-k8s \
+    --permission=package-manage \
+    --permission=package-view \
+    --ttl 2419200 # 28 days
+
+echo 'Upload token to https://github.com/canonical/juju-dashboard/settings/secrets/actions/CHARMHUB_TOKEN'
