cb225eb8f9bad86fdc9db23847dd00dec8471451
Huw Wilkins <huw.wilkins@canonical.com>
2025-08-15 10:06:45 +1000

    chore(ci): block external requests in e2e tests

--- e2e/fixtures/setup.ts
+++ e2e/fixtures/setup.ts
@@ -77,6 +77,17 @@ export const test = base.extend<Fixtures>({
       ),
     );
   },
+  page: async ({ page }, use) => {
+    await page.route("**/*", async (route, request) => {
+      // Speed up the tests by blocking requests such as the fonts on assets.ubuntu.com and icons from charmhub.io.
+      if (["media", "font", "image"].includes(request.resourceType())) {
+        await route.abort();
+      } else {
+        await route.continue();
+      }
+    });
+    await use(page);
+  },
 });
 
 export default test;
