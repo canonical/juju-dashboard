58d457bf31df79b451efd436adb178ccd7a1eed8
huwshimi <huwshimi@users.noreply.github.com>
2025-12-15 13:32:17 +1100

    chore(dev): JIMM cloud init script (#2154)

--- e2e/helpers/auth/backends/index.ts
+++ e2e/helpers/auth/backends/index.ts
@@ -1,3 +1,8 @@
 export { CreateLocalUser, LocalUser } from "./Local";
 export { CreateCandidUser, CandidUser } from "./Candid";
 export { CreateOIDCUser, OIDC, OIDCUser } from "./OIDC";
+export {
+  CreateKeycloakOIDCUser,
+  KeycloakOIDC,
+  KeycloakOIDCUser,
+} from "./Keycloak";
