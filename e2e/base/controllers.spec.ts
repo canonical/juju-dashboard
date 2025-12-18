5fdd04eb8129d2602d326c322ef2ad918393c858
Tom Anderson <tom.anderson@canonical.com>
2025-12-19 10:54:03 +1100

    count controllers in test

--- e2e/base/controllers.spec.ts
+++ e2e/base/controllers.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "@playwright/test";
 
 import { Label as PrimaryNavLabel } from "components/PrimaryNav/types";
 
-import { JujuEnv, test } from "../fixtures/setup";
+import { test } from "../fixtures/setup";
 import { ActionStack } from "../helpers/action";
 
 test.describe("Controllers", () => {
@@ -16,7 +16,7 @@ test.describe("Controllers", () => {
     await actions.rollback();
   });
 
-  test("List Controllers", async ({ page, jujuCLI, testOptions }) => {
+  test("List Controllers", async ({ page, jujuCLI }) => {
     const user = await actions.prepare((add) => {
       return add(jujuCLI.createUser());
     });
@@ -26,12 +26,7 @@ test.describe("Controllers", () => {
     });
     await expect(controllersTab).toBeInViewport();
     await controllersTab.click();
-    await expect(
-      page.getByRole("gridcell").filter({
-        hasText:
-          // If you're a non-admin in jimm the controller is displayed as "jaas".
-          testOptions.jujuEnv === JujuEnv.JIMM ? "jaas" : jujuCLI.controller,
-      }),
-    ).toBeInViewport();
+    // 1 row is the summary row, then one row for each controller.
+    await expect(page.getByRole("row")).toHaveCount(2);
   });
 });
