fca56f935145c382c00d097ca32b94d88d12a79c
Tom Anderson <tom.anderson@canonical.com>
2026-04-22 12:55:18 +1000

    feat(source): add tests for `migration-targets`

--- src/store/middleware/source/migration-targets.ts
+++ src/store/middleware/source/migration-targets.ts
@@ -1,16 +1,30 @@
 import { createPollingSource } from "data/pollingSource";
 import { listMigrationTargets } from "juju/jimm/api";
+import type { ConnectionWithFacades } from "juju/types";
 import { actions as jujuActions } from "store/juju";
 
 import { hasConnections } from "../connection/util";
 import { createSourceMiddleware } from "../source-middleware";
+
+export async function getMigrationTargets(
+  connection: ConnectionWithFacades,
+  modelUUID: string,
+): Promise<string[]> {
+  const response = await listMigrationTargets(connection, `model-${modelUUID}`);
+
+  const controllers = [];
+  for (const { uuid } of response.controllers) {
+    controllers.push(uuid);
+  }
+  return controllers;
+}
 
 export default createSourceMiddleware<
   string[],
   { wsControllerURL: string; modelUUID: string }
 >(
   "migration-targets",
-  ({ wsControllerURL: _, modelUUID: modelUUID, meta }) => {
+  ({ wsControllerURL: _, modelUUID, meta }) => {
     if (!hasConnections(meta, ["wsControllerURL"])) {
       throw new Error("connection not provided");
     }
@@ -18,18 +32,7 @@ export default createSourceMiddleware<
     const connection = meta.connections.wsControllerURL;
 
     return createPollingSource(
-      async () => {
-        const response = await listMigrationTargets(
-          connection,
-          `model-${modelUUID}`,
-        );
-
-        const controllers = [];
-        for (const { uuid } of response.controllers) {
-          controllers.push(uuid);
-        }
-        return controllers;
-      },
+      async () => getMigrationTargets(connection, modelUUID),
       { interval: { seconds: 30 } },
     );
   },
