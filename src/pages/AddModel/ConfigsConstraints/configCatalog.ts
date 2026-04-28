297a3b7aa2cbdf92a47b2a79590934ed0ceb9a08
Urvashi Sharma <urvashi.sharma@canonical.com>
2026-04-28 08:37:40 +0530

    chore: add all configs for 3.6

--- src/pages/AddModel/ConfigsConstraints/configCatalog.ts
+++ src/pages/AddModel/ConfigsConstraints/configCatalog.ts
@@ -12,6 +12,11 @@ export type CategoryDefinition = {
   }[];
 };
 
+const BOOLEAN_OPTIONS = [
+  { label: "True", value: "true" },
+  { label: "False", value: "false" },
+];
+
 export const CONFIG_CATEGORIES: CategoryDefinition[] = [
   {
     category: "Networking & Firewall",
@@ -39,10 +44,7 @@ export const CONFIG_CATEGORIES: CategoryDefinition[] = [
         defaultValue: "false",
         input: {
           type: "select",
-          options: [
-            { label: "True", value: "true" },
-            { label: "False", value: "false" },
-          ],
+          options: BOOLEAN_OPTIONS,
         },
       },
       {
@@ -74,10 +76,7 @@ export const CONFIG_CATEGORIES: CategoryDefinition[] = [
         defaultValue: "false",
         input: {
           type: "select",
-          options: [
-            { label: "True", value: "true" },
-            { label: "False", value: "false" },
-          ],
+          options: BOOLEAN_OPTIONS,
         },
       },
       {
@@ -104,10 +103,7 @@ export const CONFIG_CATEGORIES: CategoryDefinition[] = [
         defaultValue: "true",
         input: {
           type: "select",
-          options: [
-            { label: "True", value: "true" },
-            { label: "False", value: "false" },
-          ],
+          options: BOOLEAN_OPTIONS,
         },
       },
     ],
@@ -185,10 +181,7 @@ export const CONFIG_CATEGORIES: CategoryDefinition[] = [
         defaultValue: "false",
         input: {
           type: "select",
-          options: [
-            { label: "True", value: "true" },
-            { label: "False", value: "false" },
-          ],
+          options: BOOLEAN_OPTIONS,
         },
       },
       {
@@ -210,6 +203,294 @@ export const CONFIG_CATEGORIES: CategoryDefinition[] = [
       {
         label: "snap-store-proxy-url",
         description: "The URL for the defined snap store proxy",
+      },
+    ],
+  },
+  {
+    category: "Provisioning & Machines",
+    fields: [
+      {
+        label: "cloudinit-userdata",
+        description:
+          "Cloud-init user-data (in yaml format) to be added to userdata for new machines created in this model",
+      },
+      {
+        label: "container-image-metadata-defaults-disabled",
+        description:
+          "Determines whether default simplestreams sources are used for image metadata with containers",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "container-image-metadata-url",
+        description:
+          "The URL at which the metadata used to locate container OS image ids is located",
+      },
+      {
+        label: "container-image-stream",
+        description:
+          "The simplestreams stream used to identify which image ids to search when starting a container",
+        defaultValue: "released",
+      },
+      {
+        label: "container-inherit-properties",
+        description:
+          "The list of properties to be copied from the host machine to new containers created in this model (comma-separated)",
+      },
+      {
+        label: "default-base",
+        description:
+          "The default base image to use for deploying charms, will act like --base when deploying charms",
+      },
+      {
+        label: "default-series",
+        description:
+          "The default OS series to use for deploying charms (deprecated in favour of default-base)",
+      },
+      {
+        label: "enable-os-refresh-update",
+        description:
+          "Determines whether newly provisioned instances should run their respective OS's update capability",
+        defaultValue: "true",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "enable-os-upgrade",
+        description:
+          "Determines whether newly provisioned instances should run their respective OS's upgrade capability",
+        defaultValue: "true",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "image-metadata-defaults-disabled",
+        description:
+          "Determines whether default simplestreams sources are used for image metadata",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "image-metadata-url",
+        description:
+          "The URL at which the metadata used to locate OS image ids is located",
+      },
+      {
+        label: "image-stream",
+        description:
+          "The simplestreams stream used to identify which image ids to search when starting an instance",
+        defaultValue: "released",
+      },
+      {
+        label: "lxd-snap-channel",
+        description:
+          "The channel to use when installing LXD from a snap (cosmic and later)",
+        defaultValue: "5.0/stable",
+      },
+      {
+        label: "num-container-provision-workers",
+        description:
+          "The number of container provisioning workers to use per machine",
+        defaultValue: "4",
+      },
+      {
+        label: "num-provision-workers",
+        description: "The number of provisioning workers to use per model",
+        defaultValue: "16",
+      },
+      {
+        label: "project",
+        description:
+          "The name of the cloud project or namespace to use for this model",
+        defaultValue: "default",
+      },
+      {
+        label: "provisioner-harvest-mode",
+        description:
+          "Sets what to do with unknown machines. Valid values: all, none, unknown, destroyed",
+        defaultValue: "destroyed",
+        input: {
+          type: "select",
+          options: [
+            { label: "all", value: "all" },
+            { label: "none", value: "none" },
+            { label: "unknown", value: "unknown" },
+            { label: "destroyed", value: "destroyed" },
+          ],
+        },
+      },
+      {
+        label: "resource-tags",
+        description:
+          "A space-separated list of key=value pairs used to apply as tags on supported cloud models",
+        defaultValue: "{}",
+      },
+    ],
+  },
+  {
+    category: "Logging",
+    fields: [
+      {
+        label: "logforward-enabled",
+        description: "Determines whether syslog forwarding is enabled",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "logging-config",
+        description:
+          "The configuration string to use when configuring Juju agent logging",
+        defaultValue: "<root>=INFO",
+      },
+      {
+        label: "logging-output",
+        description: "The logging output destination: database and/or syslog",
+      },
+    ],
+  },
+  {
+    category: "Agents & Workloads",
+    fields: [
+      {
+        label: "agent-metadata-url",
+        description: "The URL of the private stream",
+      },
+      {
+        label: "agent-stream",
+        description: "The version of Juju to use for deploy/upgrades",
+        defaultValue: "released",
+      },
+      {
+        label: "agent-version",
+        description: "The desired Juju agent version to use",
+        defaultValue: "3.6.20",
+      },
+      {
+        label: "automatically-retry-hooks",
+        description:
+          "Determines whether the uniter should automatically retry failed hooks",
+        defaultValue: "true",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "charmhub-url",
+        description: "The url for Charmhub API calls",
+        defaultValue: "https://api.charmhub.io",
+      },
+      {
+        label: "transmit-vendor-metrics",
+        description:
+          "Determines whether metrics declared by charms deployed into this model are sent for anonymized aggregate analytics",
+        defaultValue: "true",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "update-status-hook-interval",
+        description:
+          "Sets how often to run the charm update-status hook, in human-readable time format (default 5m, range 1-60m)",
+        defaultValue: "5m",
+      },
+    ],
+  },
+  {
+    category: "Storage & Secrets",
+    fields: [
+      {
+        label: "backup-dir",
+        description: "The directory used to store the backup working directory",
+      },
+      {
+        label: "secret-backend",
+        description:
+          "The name of the secret store backend. Valid values: internal, auto, or a backend name",
+        defaultValue: "auto",
+      },
+      {
+        label: "storage-default-filesystem-source",
+        description: "The default filesystem storage source for the model",
+        defaultValue: "lxd",
+      },
+      {
+        label: "max-action-results-age",
+        description:
+          "The maximum age for action entries before they are pruned, in human-readable time format",
+        defaultValue: "336h",
+      },
+      {
+        label: "max-action-results-size",
+        description:
+          "The maximum size for the action collection, in human-readable memory format",
+        defaultValue: "5G",
+      },
+      {
+        label: "max-status-history-age",
+        description:
+          "The maximum age for status history entries before they are pruned, in human-readable time format",
+        defaultValue: "336h",
+      },
+      {
+        label: "max-status-history-size",
+        description:
+          "The maximum size for the status history collection, in human-readable memory format",
+        defaultValue: "5G",
+      },
+    ],
+  },
+  {
+    category: "Operations & Miscellaneous",
+    fields: [
+      {
+        label: "development",
+        description:
+          "Determines whether development mode is enabled for this model",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "disable-telemetry",
+        description:
+          "Determines whether telemetry collection is disabled for this model",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
+      },
+      {
+        label: "mode",
+        description: "The operation mode for model commands",
+        defaultValue: "requires-prompts",
+      },
+      {
+        label: "test-mode",
+        description: "Determines whether test mode is enabled for this model",
+        defaultValue: "false",
+        input: {
+          type: "select",
+          options: BOOLEAN_OPTIONS,
+        },
       },
     ],
   },
