c2ea18605467974e5197766cf1d8fc3ac0381e3a
Urvashi Sharma <urvashi.sharma@canonical.com>
2026-09-24 23:47:57 +0530

    chore: truncate batch destroy tooltip text

--- src/components/BulkDestroyModelDialog/ResourceCount/ResourceCount.tsx
+++ src/components/BulkDestroyModelDialog/ResourceCount/ResourceCount.tsx
@@ -5,14 +5,29 @@ import { pluralize } from "store/juju/utils/models";
 
 import { resourceIconMap, type Props } from "./types";
 
-const ResourceCount: FC<Props> = ({ resourceType, resources }) => (
-  <div className="u-flex u-flex-items-center u-flex--gap-x-small">
-    <Icon name={resourceIconMap[resourceType]} className="u-no-margin--right" />
-    {resources.length} {pluralize(resources.length, resourceType)}
-    <Tooltip message={resources.join("\n")} position="right">
-      <Icon name="information" />
-    </Tooltip>
-  </div>
-);
+const TOOLTIP_TRUNCATE_LIMIT = 9;
+
+const ResourceCount: FC<Props> = ({ resourceType, resources }) => {
+  const tooltipMessage =
+    resources.length > TOOLTIP_TRUNCATE_LIMIT
+      ? [
+          ...resources.slice(0, TOOLTIP_TRUNCATE_LIMIT),
+          `${resources.length - TOOLTIP_TRUNCATE_LIMIT} more ${pluralize(resources.length - TOOLTIP_TRUNCATE_LIMIT, resourceType).toLowerCase()}...`,
+        ].join("\n")
+      : resources.join("\n");
+
+  return (
+    <div className="u-flex u-flex-items-center u-flex--gap-x-small">
+      <Icon
+        name={resourceIconMap[resourceType]}
+        className="u-no-margin--right"
+      />
+      {resources.length} {pluralize(resources.length, resourceType)}
+      <Tooltip message={tooltipMessage} position="right">
+        <Icon name="information" />
+      </Tooltip>
+    </div>
+  );
+};
 
 export default ResourceCount;
